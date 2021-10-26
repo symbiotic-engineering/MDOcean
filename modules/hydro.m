a92183ee44b511fb978f1b988ec19c94a99b3ded
omemu <omemu@10.48.55.202>
2021-10-26 07:23:07 -0400

    Update surge force calculations, and change F_surge_hydro to F_surge.

--- modules/hydro.m
+++ modules/hydro.m
@@ -1,13 +1,27 @@
-function [F_heave, F_surge, m, D_env] = hydro(t, s, m_float, D_sft, d_WEC, N_WEC, d_farm, d_shore, rho_w, g, Hs, T, t_f)
+function [F_heave, F_surge, m, D_env] = hydro(t, s, m_float, D_sft, D_i,D_or,t_r,d_WEC, N_WEC, d_farm, d_shore, rho_w, g, Hs, T, t_f)
 
 w = 2*pi/T;         % frequency
 k = w^2 / g;        % wave number
 V_g = g /(2*w);     % group velocity
 
-r = D_sft / 2;      % radius
-A_w = pi * r^2;     % waterplane area
-draft = t_f / 2;    % estimated depth
-vol = A_w * draft;  % submerged volume
+%Surface Float Surge Geometry Calculations
+r_sf = D_sft / 2;      % surface float radius
+A_wsf = pi * r_sf^2;     % surface float waterplane area
+draft_sf = t_f / 2;    % surface float estimated depth
+vol_sf = A_wsf * draft_sf;  % surface float submerged volume
+
+%Vertical Column Surge Geometry Calculations
+r_vc=D_i/2;% vertical column radius
+A_wvc=pi*r_vc^2;%verical column waterplane area
+draft_vc=D_i*7;%vertical column depth
+vol_vc=A_wvc*draft_vc;%vertical column submerged volume
+
+%Reaction Plate Geometry Calculations
+r_rp=D_or/2;%reaction plate radius
+A_wrp=pi*r_rp^2;%reation plate waterplane area
+draft_rp=t_r;%reaction plate depth
+vol_rp=A_wrp*draft_rp;%vertical column submerged volume
+
 
 % hydrodynamic coefficients
 A       = 1/2 * rho_w * 4/3 * pi * r^3 * 0.63 * ones(1,size(s,2)); % added mass
@@ -21,11 +35,11 @@ F_radiation = -B .* s(2,:);
 F_excitation = gamma .* wave;
 F_heave = F_hydrostatic + F_radiation + F_excitation;
 
-F_surge = 2 * k * rho_w * g * vol * (1 - exp(k*draft));
+%Surge Force Per Geometry Calculations
+F_surge = 2 * (Hs/2) * rho_w * g * (vol_sf+vol_vc+vol_rp) * (1 - exp(-k*(draft_sf+draft_vc+draft_rp)));
 
 m = m_float + A;
 
 D_env = 0.5 * ones(1,size(s,2)); % in reality this is a function of d_farm and forces
-
 end
 
