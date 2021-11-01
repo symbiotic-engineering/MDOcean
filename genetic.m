8c1fb45122ebc22dfe339860ddc7ccb130b9db6e
Olivia Murphy <om66@cornell.edu>
2021-11-01 19:50:34 -0400

    git commit -m 'first attempt at scaling  plus metacentric height addition to gentic.m'

--- genetic.m
+++ genetic.m
@@ -1,21 +1,24 @@
 clc, clear, close all
 p = parameters();
-p.M = 1; % hardcode steel for now - run a for-loop
 
-FitFcn=@(X)simulation([X(1) X(2) X(3) 0 X(4) X(5)],zeros(20,21),p);
-nvars=5;
-lb=[0,0,0,1,1e6];
-ub=[50,1,50,100,1e8];
+w_max = 2*pi/p.T(find(any(p.JPD > 0),1,'first')); % max wave frequency that has any energy
+w_min = 2*pi/p.T(find(any(p.JPD > 0),1,'last'));  % min wave frequency that has any energy
+
+FitFcn=@(X)simulation(X,p);
+nvars=7;
+lb=[0,0,0,1,1,1,w_min];
+ub=[50,1,50,3,100,100,w_max];
 options = optimoptions('ga', 'PlotFcn', {@gaplotbestf}, 'Display', 'iter');
-[X,fval]=ga(FitFcn,nvars,[],[],[],[],lb,ub,@mycon,options);
+intcon = 4;
+[X,fval]=ga(FitFcn,nvars,[],[],[],[],lb,ub,@mycon,intcon,options);
 
 
 %Constraints
 function [c,ceq] = mycon(X)
 p=parameters();
 [LCOE, D_env, B, FOS1Y, FOS2Y, ...
-            FOS3Y, FOS_buckling, ...
-            ~, ~, F_pt_unsat] = simulation ([X(1) X(2) X(3) 0 X(4) X(5)],zeros(20,21),p);
+            FOS3Y, FOS_buckling,GM ...
+            ~, ~, F_pt_unsat] = simulation (X,p);
 
 c(1)=FOS1Y(1)-p.FOS_min; %<=0;
 c(2)=FOS1Y(2)-p.FOS_min;%<=0;
@@ -25,6 +28,7 @@ c(5)=FOS3Y(1)-p.FOS_min; %<=0;
 c(6)=FOS3Y(2)-p.FOS_min; %<=0;
 c(7)=FOS_buckling(1)-p.FOS_min; %<=0;
 c(8)=FOS_buckling(2)-p.FOS_min; %<=0;
-c(9)=B-p.B_min; %<=0; 
+c(9)=B-p.B_min; %<=0;
+c(10)=GM;
 ceq=[];
 end
