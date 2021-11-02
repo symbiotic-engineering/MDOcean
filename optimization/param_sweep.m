42c4ec02fb01ec5fff4b5bcec0ef67634ea3fb20
Rebecca McCabe <rmccabe@mit.edu>
2021-11-02 08:52:13 -0400

    parameter sweep

---
+++ optimization/param_sweep.m
@@ -0,0 +1,27 @@
+clear;clc;close all
+
+vars = {'F_max','FCR','t_sft'};
+ratios = .5 : .1 : 1.5;
+b = var_bounds(parameters());
+x0 = struct('D_sft',b.D_sft_nom,'D_i_ratio',b.D_i_ratio_nom,'D_or',...
+        b.D_or_nom,'N_WEC',b.N_WEC_nom,'D_int',b.D_int_nom,'w_n',b.w_n_nom);
+   
+LCOE = zeros(length(vars),length(ratios));
+for i=1:length(vars)
+    p = parameters();
+    var_nom = p.(vars{i});
+    for j=1:length(ratios)
+        p.(vars{i}) = ratios(j) * var_nom;
+        [X_opt, LCOE(i,j), flag] = gradient_optim(x0,p,b);
+    end   
+end
+%%
+figure
+plot(ratios,LCOE)
+xlabel('Ratio from nominal')
+ylabel('LCOE')
+legend({'F_{max}','FCR','t_{sft}'})
+improvePlot
+grid on
+%%
+slope = LCOE(:,1) - LCOE(:,end)
