22a2a8ce099f0eeaa216f656099f91528451725a
Rebecca McCabe <rmccabe@mit.edu>
2021-11-02 06:55:30 -0400

    add multipe x0s for gradient optim

---
+++ gradient_mult_x0.m
@@ -0,0 +1,47 @@
+clear;clc;close all
+
+p = parameters();
+b = var_bounds(p);
+
+num_runs = 20;
+LCOE = Inf(num_runs,1);
+X_opt = zeros(num_runs,7);
+flag = zeros(num_runs,1);	
+x0s = struct('D_sft',[],'D_i_ratio',[],'D_or',[],'N_WEC',[],'D_int',[],'w_n',[]);
+
+for i = 1:num_runs
+    [~,x0] = random_x0(b);
+    x0s(i) = x0;
+    [X_opt(i,:), LCOE(i), flag(i)] = gradient_optim(x0,p,b);	
+end
+
+% create table for display	
+var_names = {'D_sft',...    % outer diameter of float (m)	
+            'D_i/D_sft',... % inner diameter ratio of float (m)	
+            'D_or',...      % outer diameter of reaction plate (m)	
+            'M',...         % material (-)	
+            'N_WEC',...     % number of WECs in array (-)	
+            'D_int',...     % internal damping of controller (Ns/m)	
+            'w_n'};         % natural frequency (rad/s)
+        %%
+results = struct2table(x0s);
+results = addvars(results, round(LCOE,1), flag,  ...	
+    'NewVariableNames', {'LCOE ($/kWh)','Flag'});
+
+var_names = {'D_sft',...    % outer diameter of float (m)	
+            'D_i_ratio',... % inner diameter ratio of float (m)	
+            'D_or',...      % outer diameter of reaction plate (m)	
+            'M',...         % material (-)	
+            'N_WEC',...     % number of WECs in array (-)	
+            'D_int',...     % internal damping of controller (Ns/m)	
+            'w_n'};         % natural frequency (rad/s)
+for i=1:length(var_names)
+    if i~= 4
+    results = addvars(results, X_opt(:,i), 'NewVariableNames', ...
+                    [var_names{i} '_opt'], 'After', var_names{i});
+    end
+end
+
+results = sortrows(results,'Flag','descend');
+results.Variables =  round(results.Variables,1);
+disp(results)
--- gradient_optim.m
+++ gradient_optim.m
@@ -1,7 +1,19 @@
-clear;clc;close all
+function [X_opt, opt_LCOE, flag] = gradient_optim(x0,p,b)
 
-p = parameters();
-b = var_bounds(p);
+if nargin == 0
+    clc;close all
+    p = parameters();
+    b = var_bounds(p);
+    x0 = struct('D_sft',b.D_sft_nom,'D_i_ratio',b.D_i_ratio_nom,'D_or',...
+        b.D_or_nom,'N_WEC',b.N_WEC_nom,'D_int',b.D_int_nom,'w_n',b.w_n_nom);
+    display = 'iter';
+    plotfn = @optimplotfval;
+    ploton = true;
+else
+    display = 'off';
+    plotfn = [];
+    ploton = false;
+end
 
 D_sft       = optimvar('D_sft',     [1 1],'LowerBound',b.D_sft_min,    'UpperBound',b.D_sft_max);
 D_i_ratio   = optimvar('D_i_ratio', [1 1],'LowerBound',b.D_i_ratio_min,'UpperBound',b.D_i_ratio_max);
@@ -10,21 +22,18 @@ N_WEC       = optimvar('N_WEC',     [1 1],'LowerBound',b.N_WEC_min,    'UpperBou
 D_int       = optimvar('D_int',     [1 1],'LowerBound',b.D_int_min,    'UpperBound',b.D_int_max);
 w_n         = optimvar('w_n',       [1 1],'LowerBound',b.w_n_min,      'UpperBound',b.w_n_max);
 
-x0 = struct('D_sft',b.D_sft_nom,'D_i_ratio',b.D_i_ratio_nom,'D_or',...
-    b.D_or_nom,'N_WEC',b.N_WEC_nom,'D_int',b.D_int_nom,'w_n',b.w_n_nom);
-
-opts = optimoptions('fmincon',	'Display','iter',...
+opts = optimoptions('fmincon',	'Display',display,...
                                 'Algorithm','sqp',...
-                                'PlotFcn',@optimplotfval);
-    
-for matl = 1:2:3 %b.M_min : b.M_max
+                                'PlotFcn',plotfn);
+                            
+for matl = 1%1:2:3 %b.M_min : b.M_max
     X = [D_sft D_i_ratio D_or matl N_WEC D_int w_n];
 
     [LCOE, D_env, B, FOS1Y, FOS2Y, FOS3Y, ...
-            FOS_buckling, GM, ~, ~] = fcn2optimexpr(@simulation,X,p);%simulation(X, p);
+            FOS_buckling, GM, P_elec, ~] = fcn2optimexpr(@simulation,X,p);%simulation(X, p);
 
     prob = optimproblem('Objective',LCOE);
-    
+
     % Structures Constraints
     prob.Constraints.B              = B >= p.B_min;
     prob.Constraints.FOS1YH         = FOS1Y(1) >= p.FOS_min;
@@ -36,12 +45,17 @@ for matl = 1:2:3 %b.M_min : b.M_max
     prob.Constraints.FOS_bucklingH  = FOS_buckling(1) >= p.FOS_min;
     prob.Constraints.FOS_bucklingP  = FOS_buckling(2) >= p.FOS_min;
     prob.Constraints.GM             = GM >= 0;
+    prob.Constraints.P_positive     = P_elec >= 0;
 
     %show(prob)
     %% Run optimization
-    [opt_x, opt_LCOE, ~,~,lambda] = solve(prob,x0,'Options',opts);
-    
+    [opt_x, opt_LCOE, flag] = solve(prob,x0,'Options',opts);
+
     %% Post process
-    X_opt = [opt_x.D_sft opt_x.D_i_ratio opt_x.D_or matl opt_x.N_WEC opt_x.D_int opt_x.w_n]
-    plot_power_matrix(X_opt,p)
+    X_opt = [opt_x.D_sft opt_x.D_i_ratio opt_x.D_or matl opt_x.N_WEC opt_x.D_int opt_x.w_n];
+    if ploton
+        plot_power_matrix(X_opt,p)
+    end
+end
+
 end
