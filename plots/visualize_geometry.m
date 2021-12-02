b907a0f166a935403e00877d029802f471aa58fc
Rebecca McCabe <rmccabe@mit.edu>
2021-12-02 03:29:28 -0500

    waves behind, formatting for mini plot insertion

--- plots/visualize_geometry.m
+++ plots/visualize_geometry.m
@@ -1,22 +1,23 @@
-close all
-p = parameters();
-b = var_bounds(p);
-x = [b.D_sft_nom, b.D_i_ratio_nom, b.D_or_nom, b.M_nom, b.N_WEC_nom, b.D_int_nom, b.w_n_nom];
-vis_geometry(x,p)
+% close all
+% p = parameters();
+% b = var_bounds(p);
+% x = [b.D_sft_nom, b.D_i_ratio_nom, b.D_or_nom, b.M_nom, b.N_WEC_nom, b.D_int_nom, b.w_n_nom];
+% visualize_geometry(x,p)
 
-function vis_geometry(x,p)
+function visualize_geometry(x,p,mini)
+
+if nargin<3
+    mini = false; % whether to adjust formatting for a mini plot inset
+end
 
 [D_sft,D_i_ratio,D_or,~,N_WEC,D_int,w_n] = deal(x(1),x(2),x(3),x(4),x(5),x(6),x(7));
 
-figure
+if ~mini
+    figure
+end
 t_f = D_sft / 4;
 D_i = D_i_ratio * D_sft;
 h = D_i * 7;
-
-% WEC
-center_rect([0 0 D_sft t_f],'r')            % float
-center_rect([0 (t_f-h)/2+5 D_i h],'b')      % vert col
-center_rect([0 -h+5+t_f/2 D_or p.t_r],'k')  % reaction plate
 
 % waves
 x = linspace(-30,30,100);
@@ -25,9 +26,20 @@ T = 7.5;
 hold on
 plot(x,Hs*cos(x*2*pi/T),'c')
 
-grid on
+% WEC
+center_rect([0 (t_f-h)/2+5 D_i h],'b')      % vert col
+center_rect([0 -h+5+t_f/2 D_or p.t_r],'k')  % reaction plate
+center_rect([0 0 D_sft t_f],'r')            % float
+
 axis equal
-improvePlot
+if ~mini
+    improvePlot
+else
+    set(gcf, 'Color', 'white');
+end
+grid on
+ylim([-60 10])
+xlim([-30 30])
 
 end
 
