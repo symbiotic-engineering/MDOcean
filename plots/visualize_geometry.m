682f05a7fd4838d8cb4d8a9904c4172eb747600c
Rebecca McCabe <rmccabe@mit.edu>
2021-12-02 18:45:55 -0500

    visual comparison plot of all designs

--- plots/visualize_geometry.m
+++ plots/visualize_geometry.m
@@ -4,15 +4,23 @@
 % x = [b.D_sft_nom, b.D_i_ratio_nom, b.D_or_nom, b.M_nom, b.N_WEC_nom, b.D_int_nom, b.w_n_nom];
 % visualize_geometry(x,p)
 
-function visualize_geometry(x,p,mini)
+function visualize_geometry(x,p,mini,color)
 
 if nargin<3
     mini = false; % whether to adjust formatting for a mini plot inset
+    compare = false;
+end
+if nargin<4
+    color = {'b','k','r'};
+    compare = false;
+else
+    compare = true;
+    color = {color,color,color};
 end
 
 [D_sft,D_i_ratio,D_or,~,N_WEC,D_int,w_n] = deal(x(1),x(2),x(3),x(4),x(5),x(6),x(7));
 
-if ~mini
+if ~mini && ~compare
     figure
 end
 t_f = D_sft / 4;
@@ -24,12 +32,15 @@ x = linspace(-30,30,100);
 Hs = 1.5;
 T = 7.5;
 hold on
-plot(x,Hs*cos(x*2*pi/T),'c')
+waves = plot(x,Hs*cos(x*2*pi/T),'c');
 
 % WEC
-center_rect([0 (t_f-h)/2+5 D_i h],'b')      % vert col
-center_rect([0 -h+5+t_f/2 D_or p.t_r],'k')  % reaction plate
-center_rect([0 0 D_sft t_f],'r')            % float
+center_rect([0 (t_f-h)/2+5 D_i h],color{1})      % vert col
+center_rect([0 -h+5+t_f/2 D_or p.t_r],color{2})  % reaction plate
+center_rect([0 0 D_sft t_f],color{3})            % float
+
+% for legend
+plot(NaN,NaN,color{1})
 
 axis equal
 if ~mini
@@ -40,6 +51,7 @@ end
 grid on
 ylim([-80 10])
 xlim([-30 30])
+set(waves,'HandleVisibility','off')
 
 end
 
