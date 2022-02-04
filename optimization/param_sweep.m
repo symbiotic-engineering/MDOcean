7b38a2a28e445796c4767a2a32a312f23b3a7e90
Rebecca McCabe <rmccabe@mit.edu>
2022-02-04 11:17:11 -0500

    output both objectives

--- optimization/param_sweep.m
+++ optimization/param_sweep.m
@@ -1,18 +1,22 @@
 clear;clc;close all
 
-vars = {'F_max','FCR','t_sft'};
-ratios = .5 : .1 : 1.5;
+vars = {'F_max'}; % {'F_max','FCR','t_sft'};
+ratios = [.1, 1:.5:8]; % .5 : .1 : 1.5
 b = var_bounds(parameters());
 x0 = struct('D_sft',b.D_sft_nom,'D_i_ratio',b.D_i_ratio_nom,'D_or',...
         b.D_or_nom,'N_WEC',b.N_WEC_nom,'D_int',b.D_int_nom,'w_n',b.w_n_nom);
    
-LCOE = zeros(length(vars),length(ratios));
+LCOE  = zeros(length(vars),length(ratios));
+P_var = zeros(length(vars),length(ratios));
+
 for i=1:length(vars)
     p = parameters();
     var_nom = p.(vars{i});
     for j=1:length(ratios)
         p.(vars{i}) = ratios(j) * var_nom;
-        [X_opt, LCOE(i,j), flag] = gradient_optim(x0,p,b);
+        [~, obj_opt, flag] = gradient_optim(x0,p,b);
+        LCOE(i,j) = obj_opt(1);
+        P_var(i,j) = obj_opt(2);
     end   
 end
 %%
@@ -20,7 +24,15 @@ figure
 plot(ratios,LCOE)
 xlabel('Ratio from nominal')
 ylabel('LCOE')
-legend({'F_{max}','FCR','t_{sft}'})
+legend(vars)
+improvePlot
+grid on
+
+figure
+plot(ratios,P_var)
+xlabel('Ratio from nominal')
+ylabel('Power Variation')
+legend(vars)
 improvePlot
 grid on
 %%
