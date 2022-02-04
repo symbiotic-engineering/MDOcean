e1063febc5493a4b5c3d2e8284d2c8cb15607d06
Rebecca McCabe <rmccabe@mit.edu>
2022-02-04 13:31:14 -0500

    DOE pareto graph with different sweep values

--- experiments.m
+++ experiments.m
@@ -1,16 +1,31 @@
 clear;clc;close all
 
 p = parameters();
+b = var_bounds(p);
 
-X = [ 20 10 30;     % outer diameter of float	
-      .3 .1 .5;     % inner diameter ratio of float	
-      30 15 45;     % outer diameter of reaction plate	
-      1 2 3;        % material	
-      10 1 20;      % Number of WECs in array	
-      10 5 50       % D_int	
-      2*pi/7 2*pi/8 2*pi/9
-        ];	
-    	
+% X = [ 20 10 30;     % outer diameter of float	
+%       .3 .1 .5;     % inner diameter ratio of float	
+%       30 15 45;     % outer diameter of reaction plate	
+%       1 2 3;        % material	
+%       10 1 20;      % Number of WECs in array	
+%       10 5 50       % D_int	
+%       2*pi/7 2*pi/8 2*pi/9
+%         ];	
+
+n = 10;
+% X = [ b.D_sft_nom, linspace(b.D_sft_min, b.D_sft_max, n);
+%       b.D_i_ratio_nom, linspace(b.D_i_ratio_min, b.D_i_ratio_max, n);
+%       b.D_or_nom, linspace(b.D_or_min, b.D_or_max, n);
+%       1, ones([1 n]);
+%       b.N_WEC_nom, linspace(b.N_WEC_min, b.N_WEC_max, n);
+%       b.D_int_nom, linspace(b.D_int_min, b.D_int_max, n);
+%       b.w_n_nom, linspace(b.w_n_min, b.w_n_max, n) ];
+
+ratios = logspace(-1,1,n);
+ratios = [1, ratios(ratios~=1)];
+X =  [b.D_sft_nom, b.D_i_ratio_nom, b.D_or_nom, 1, b.N_WEC_nom, b.D_int_nom, b.w_n_nom]' * ratios;
+X(4,:) = ones(1,n+1);
+
 X_nom = X(:,1);	
 design_size = size(X);	
 var_num = design_size(1);	
@@ -53,6 +68,7 @@ for i = 1:var_num
 end	
 [LCOE_op, ~, ~, B_op, FOS1Y_op,FOS2Y_op,FOS3Y_op,FOS_buckling_op,power_op] = simulation(recommended(:,1),p);	
 [op_feasible, CC] = is_feasible(power_op, B_op, FOS1Y_op, power_op, p);	
+
 % create table for display	
 var_names = {'D_sft',...    % outer diameter of float (m)	
             'D_i/D_sft',... % inner diameter ratio of float (m)	
@@ -61,9 +77,25 @@ var_names = {'D_sft',...    % outer diameter of float (m)
             'N_WEC',...     % number of WECs in array (-)	
             'D_int',...     % internal damping of controller (Ns/m)	
             'w_n'};         % natural frequency (rad/s)
+var_names_pretty = {'D_{sft}',...    % outer diameter of float (m)	
+            'D_i/D_{sft}',... % inner diameter ratio of float (m)	
+            'D_{or}',...      % outer diameter of reaction plate (m)	
+            'M',...         % material (-)	
+            'N_{WEC}',...     % number of WECs in array (-)	
+            'D_{int}',...     % internal damping of controller (Ns/m)	
+            'w_n'};         % natural frequency (rad/s)
 results = array2table(X_ins, 'VariableNames', var_names);	
 LCOE = LCOE';
 P_var = P_var';
 results = addvars(results, round(LCOE(LCOE~=Inf),1), round(power/1e3), round(P_var(P_var~=Inf)), FOS, failed, ...	
     'NewVariableNames', {'LCOE ($/kWh)','Power (kW)','Power Variance (%)','FOS (-)','ConstraintsFailed'});	
 disp(results)
+
+% create pareto front
+figure
+plot(LCOE, P_var, '*')
+xlabel('LCOE')
+ylabel('P_{var}')
+title('Design of Experiments Pareto Front')
+legend(var_names_pretty)
+improvePlot
