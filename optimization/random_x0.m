8b3ee4f4c5b9cad403ddea4dce838857663c4d74
Rebecca McCabe <rmccabe@mit.edu>
2022-02-15 00:03:24 -0500

    add design heuristics distance along pareto graph

--- optimization/random_x0.m
+++ optimization/random_x0.m
@@ -1,7 +1,7 @@
 function [x0,x0_struct] = random_x0(b)
 
-mins = [b.D_sft_min, b.D_i_ratio_min, b.D_or_ratio_min, b.M_min, b.N_WEC_min, b.D_int_min, b.w_n_min];
-maxs = [b.D_sft_max, b.D_i_ratio_max, b.D_or_ratio_max, b.M_max, b.N_WEC_max, b.D_int_max, b.w_n_max];
+mins = [b.D_sft_min, b.D_i_ratio_min, b.D_or_ratio_min, b.M_min, b.F_max_min, b.D_int_min, b.w_n_min];
+maxs = [b.D_sft_max, b.D_i_ratio_max, b.D_or_ratio_max, b.M_max, b.F_max_max, b.D_int_max, b.w_n_max];
 
 r = rand(1,7);
 
