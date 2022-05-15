485f5e694ea11a9b0d7e4db836af648a86faa55e
Rebecca McCabe <chessgirl2400@gmail.com>
2022-05-15 04:16:19 -0400

    add F_max constraint

--- optimization/gradient_optim.m
+++ optimization/gradient_optim.m
@@ -5,9 +5,7 @@ if nargin == 0
     clc;close all
     p = parameters();
     b = var_bounds(p);
-    x0_input = struct('D_f',b.D_f_nom,'D_s_ratio',b.D_s_ratio_nom,...
-        'h_f_ratio',b.h_f_ratio_nom,'T_s_ratio',b.T_s_ratio_nom,...
-        'F_max',b.F_max_nom,'D_int',b.D_int_nom,'w_n',b.w_n_nom);
+    x0_input = b.X_nom_struct;
     display = 'iter';
     plotfn = @optimplotfval;
     ploton = true;
@@ -79,6 +77,7 @@ function [Xs_opt, objs_opt, flags] = optimize_both_objectives(X,p,b,x0_input,opt
         prob.Constraints.Damping                = D_d/p.D_d_min >= 1;
         prob.Constraints.Spar_height            = g(16) >= 0;
         prob.Constraints.LCOE_max               = g(17) >= 0;
+        prob.Constraints.F_max                  = g(18) >= 0;
 
         %show(prob)
 
