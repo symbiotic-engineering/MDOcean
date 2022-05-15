1dadb133b09a56f4fbc5dbb0b3802476d669700a
Rebecca McCabe <chessgirl2400@gmail.com>
2022-05-15 16:24:04 -0400

    statistics for multiple x0 used in paper

--- optimization/gradient_mult_x0.m
+++ optimization/gradient_mult_x0.m
@@ -3,15 +3,15 @@ clear;clc;close all
 p = parameters();
 b = var_bounds(p);
 
-num_runs = 25;
+num_runs = 100;
 objs = Inf(num_runs,2);
 X_opt = zeros(num_runs,8,2);
 flags = zeros(num_runs,2);	
 x0s = struct('D_f',[],'D_s_ratio',[],'h_f_ratio',[],'T_s_ratio',[],'F_max',[],'D_int',[],'w_n',[]);
 
 % nominal ICs
-[X_opt(1,:,:), objs(1,:), flags(1,:)] = gradient_optim(b.X_nom_struct,p,b);	
-x0s(1) = b.X_nom_struct;
+[X_opt(1,:,:), objs(1,:), flags(1,:)] = gradient_optim(b.X_start_struct,p,b);	
+x0s(1) = b.X_start_struct;
 
 % 20 random ICs
 for i = 2:num_runs
@@ -45,3 +45,20 @@ end
 results = sortrows(results,{'Flag','Objs'},'descend');
 results.Variables =  round(results.Variables,1);
 disp(results)
+
+%% summary statistics
+converged = flags > 0;
+kkt = flags == 1;
+tol = 0.001;
+optimal = abs(objs - min(objs)) < tol; % this assumes that solutions converging 
+% to the same objective have the same optimal DVs, which is not guaranteed 
+% but seems correct in this case by inspection of the table above
+
+optimal_and_converged = converged & optimal;
+optimal_and_kkt = kkt & optimal;
+
+percent_converged = sum(converged) / num_runs
+percent_kkt = sum(kkt) / num_runs
+percent_optimal = sum(optimal) / num_runs
+percent_optimal_given_converged = sum(optimal_and_converged) ./ sum(converged)
+percent_optimal_given_kkt = sum(optimal_and_kkt) ./ sum(kkt)
