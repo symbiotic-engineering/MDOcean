8cc8b1f697e30141f5176d5793054785a9cebd30
rebeccamccabe <chessgirl2400@gmail.com>
2022-06-30 19:47:26 -0400

    fix buckling end condition and hydrostatic pressure

--- simulation/modules/structures.m
+++ simulation/modules/structures.m
@@ -3,13 +3,13 @@ function [FOS1Y,FOS2Y,FOS3Y,FOS_buckling] = structures(...
             sigma_y, A_c, A_lat_sub, r_over_t, I, E)
 
     %% Stress calculations
-    depth = T_s; % max depth
+    depth = [0 T_s T_s]; % max depth
     
     P_hydrostatic = rho_w * g * depth;
     sigma_surge = F_surge ./ A_lat_sub;
     
     sigma_rr = P_hydrostatic + sigma_surge;     % radial compression
-    sigma_tt = P_hydrostatic * r_over_t;        % hoop stress
+    sigma_tt = P_hydrostatic .* r_over_t;       % hoop stress
     sigma_zz = F_heave ./ A_c;                  % axial compression
     sigma_rt = sigma_surge;                     % shear
     sigma_tz = [0 0 0];
@@ -27,7 +27,7 @@ function [FOS1Y,FOS2Y,FOS3Y,FOS_buckling] = structures(...
     sigma_vm = von_mises(sigma_rr, sigma_tt, sigma_zz, sigma_rt, sigma_tz, sigma_zr);
     
     %% Buckling calculation
-    K = 1; % free-free - is this correct?
+    K = 2; % fixed-free - top is fixed by float angular stiffness, bottom is free
     L = h_s;
     F_buckling = pi^2 * E(M) * I(2) / (K*L)^2;
     
