dd0148f86e3a97e735285d3a51b18c6228bd3ecb
rebeccamccabe <chessgirl2400@gmail.com>
2022-07-27 20:09:39 -0400

    adjust econ module to have correct N WEC scaling

--- simulation/modules/econ.m
+++ simulation/modules/econ.m
@@ -4,19 +4,22 @@ function [LCOE, capex, opex] = econ(m_m, M, cost_m, N_WEC, P_elec, FCR)
 structural_cost = m_m.* cost_m;
 devicestructure =  N_WEC * structural_cost(M);
 
+% costs taken from 'CBS (Total)' tab of the RM3 cost breakdown structure
+% https://catalog.data.gov/ne/dataset/reference-model-3-cost-breakdown-rm3-wave-point-absorber
 development     = 4553000;
 infrastructure  = 990000;
 mooring         = N_WEC * 525000;
 pto             = N_WEC * 623000;
 profitmargin    = 356000;
-installation    = N_WEC * 5909000;
+installation    = 5909000;
 contingency     = 1590000;
+
 operations      = N_WEC * 27000;
 postinstall     = 710000;
 shoreoperations = 142000;
 replacement     = N_WEC * 54000;
 consumables     = N_WEC * 8000;
-insurance       = N_WEC * 227000;
+insurance       = 227000;
 
 hr_per_yr = 8766;
 aep = N_WEC * P_elec*hr_per_yr/1000; % annual energy production: W to kWh per year
