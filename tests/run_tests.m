1efcbd1209f31c9d5fd3e0ed2785e3e949ff532e
rebeccamccabe <chessgirl2400@gmail.com>
2022-10-02 19:15:47 -0400

    use personal test script rather than mathworks default

--- tests/run_tests.m
+++ tests/run_tests.m
@@ -1,15 +1,30 @@
 import matlab.unittest.plugins.CodeCoveragePlugin
 import matlab.unittest.plugins.codecoverage.CoverageReport
+import matlab.unittest.TestRunner;
+import matlab.unittest.plugins.XMLPlugin;
+import matlab.unittest.plugins.codecoverage.CoberturaFormat;
 
 sourceCodeFolder = "mdocean";
 addpath(genpath(sourceCodeFolder))
 suite = testsuite("tests");
 runner = testrunner("textoutput");
 
-reportFolder = "coverageReport";
+mkdir('code-coverage');
+mkdir('test-results');
+
+reportFolder = "code-coverage/coverageReport";
 reportFormat = CoverageReport(reportFolder);
 
 p = CodeCoveragePlugin.forFolder(sourceCodeFolder,"Producing",reportFormat,"IncludingSubfolders",true);
-runner.addPlugin(p)
+p = CodeCoveragePlugin.forFolder({'.'}, 'IncludingSubfolders', true, 'Producing', CoberturaFormat('code-coverage/coverage.xml'));
+p2 = XMLPlugin.producingJUnitFormat('test-results/results.xml');
+
+runner.addPlugin(p);
+runner.addPlugin(p2);
+
 results = runner.run(suite);
+display(results);
+
 open(fullfile("coverageReport","index.html"))
+
+assertSuccess(results);
