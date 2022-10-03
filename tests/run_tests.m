8ba1854b2892dfd384016ce8fee4b16ff3a468d3
rebeccamccabe <chessgirl2400@gmail.com>
2022-10-02 22:16:39 -0400

    try going back to server test with source code folder added

--- tests/run_tests.m
+++ tests/run_tests.m
@@ -15,8 +15,7 @@ mkdir('test-results');
 reportFolder = "code-coverage/coverageReport";
 reportFormat = CoverageReport(reportFolder);
 
-p = CodeCoveragePlugin.forFolder(sourceCodeFolder,"Producing",reportFormat,"IncludingSubfolders",true);
-p = CodeCoveragePlugin.forFolder({'.'}, 'IncludingSubfolders', true, 'Producing', CoberturaFormat('code-coverage/coverage.xml'));
+p = CodeCoveragePlugin.forFolder({'.'}, 'IncludingSubfolders', true, 'Producing', [CoberturaFormat('code-coverage/coverage.xml') reportFormat]);
 p2 = XMLPlugin.producingJUnitFormat('test-results/results.xml');
 
 runner.addPlugin(p);
@@ -25,6 +24,6 @@ runner.addPlugin(p2);
 results = runner.run(suite);
 display(results);
 
-open(fullfile("coverageReport","index.html"))
+open(fullfile("code-coverage/coverageReport","index.html"))
 
 assertSuccess(results);
