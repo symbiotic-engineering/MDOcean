6e9303ad66d7fafd0ba5e4ecfcebd125f5f95b84
rebeccamccabe <chessgirl2400@gmail.com>
2022-10-02 23:58:23 -0400

    add local test outputting results as pdf

--- tests/run_tests.m
+++ tests/run_tests.m
@@ -3,27 +3,30 @@ import matlab.unittest.plugins.codecoverage.CoverageReport
 import matlab.unittest.TestRunner;
 import matlab.unittest.plugins.XMLPlugin;
 import matlab.unittest.plugins.codecoverage.CoberturaFormat;
+import matlab.unittest.plugins.TestReportPlugin
 
-sourceCodeFolder = "mdocean";
+sourceCodeFolder = 'mdocean';
 addpath(genpath(sourceCodeFolder))
-suite = testsuite("tests");
-runner = testrunner("textoutput");
+suite = testsuite('tests');
+runner = testrunner('textoutput');
 
 mkdir('code-coverage');
 mkdir('test-results');
 
-reportFolder = "code-coverage/coverageReport";
-reportFormat = CoverageReport(reportFolder);
+reportFormat = [CoverageReport('code-coverage/coverageReport') CoberturaFormat('code-coverage/coverage.xml')];
 
-p = CodeCoveragePlugin.forFolder({'.'}, 'IncludingSubfolders', true, 'Producing', [CoberturaFormat('code-coverage/coverage.xml') reportFormat]);
+p1 = CodeCoveragePlugin.forFolder({sourceCodeFolder}, 'IncludingSubfolders', true, 'Producing', reportFormat);
 p2 = XMLPlugin.producingJUnitFormat('test-results/results.xml');
+p3 = TestReportPlugin.producingPDF('test-results/testreport.pdf');
 
-runner.addPlugin(p);
+runner.addPlugin(p1);
 runner.addPlugin(p2);
+runner.addPlugin(p3);
 
 results = runner.run(suite);
+
+open('code-coverage/coverageReport/index.html')
+open('test-results/testreport.pdf')
+
 display(results);
-
-open(fullfile("code-coverage/coverageReport","index.html"))
-
 assertSuccess(results);
