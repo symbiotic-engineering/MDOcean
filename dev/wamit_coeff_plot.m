25069a411b8e6e3a67cda0f361da51a4c7a7fc0c
rebeccamccabe <chessgirl2400@gmail.com>
2022-10-10 23:53:33 -0400

    use new coeff function and calculate mean error

--- dev/wamit_coeff_plot.m
+++ dev/wamit_coeff_plot.m
@@ -10,18 +10,14 @@ gamma = hydro.ex_ma(3,1,:);
 gamma = gamma(:);
 
 r = 10;
-fudge_factor =  5;
 draft = 2;
 g = 9.8;
-rho_w = 1000;
 
 k = w.^2 / g;
-V_g = g ./(2*w);
 
-A_MDOcean = ones(size(w))* 1/2 * 4/3 * pi * r^3 * 0.63;
-r_k_term = r^2 - 1/8 * k.^2 * r^4 + 1/192 * k.^4 * r^6 - 1/9216 * k.^6 * r^8;
-gamma_MDOcean   = pi * exp(-k * draft * fudge_factor) .* r_k_term; 
-B_MDOcean = k/2 .* gamma_MDOcean.^2;
+[A_MDOcean,B_MDOcean,gamma_MDOcean] = get_hydro_coeffs(r,k,draft);
+A_MDOcean = ones(size(w))* A_MDOcean;
+
 %% coeff comparison validation figure
 figure
 plot(w,A,'--',w,B,'--',w,gamma,'--','LineWidth',3,'HandleVisibility','off')
@@ -35,6 +31,14 @@ legend('Added Mass A/\rho','Radiation Damping B/(\rho\omega)','Excitation Force 
 title('Normalized Hydrodynamic Coefficients')
 xlabel('Wave frequency \omega (rad/s)')
 improvePlot
+
+%% mean error
+err_A = abs(A - A_MDOcean') ./ A;
+err_B = abs(B - B_MDOcean') ./ B;
+err_G = abs(gamma - gamma_MDOcean') ./ gamma;
+mean(err_A(w<1.5))
+mean(err_B(w<1.5))
+mean(err_G(w<1.5))
 
 %% R^2 table
 
