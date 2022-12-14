7b3d1df0a6b24b98ed881ea3a3fb241d4a847090
Rebecca McCabe <chessgirl2400@gmail.com>
2022-12-13 19:43:01 -0500

    refactor to use a big for loop for each geometry

--- dev/Chau_Yeung_2010.m
+++ dev/Chau_Yeung_2010.m
@@ -67,15 +67,15 @@ Lambda_n(n) = subs(Lambda_k, k, n);
 Z_n_e(n) = subs(Z_k_e, k, n);
 
 % equation 22 in old 1981 paper, applied to boundary 2-e
-dz_2 = 1 - d2/h;
+dz_2 = h - d2;
 match_2e_potential = C_1n_2(n) * subs(R_1n_2(n),r,a2) + C_2n_2(n) * subs(R_2n_2(n),r,a2) == ...
-    B_n(n) * subs(Lambda_n(n),r,a2) * dz_2 - int(subs(phi_p_i2,r,a2) * Z_n_i2(n), z, 0, dz_2);
+    B_n(n) * subs(Lambda_n(n),r,a2) * dz_2 - int(subs(phi_p_i2,r,a2) * Z_n_i2(n), z, -h, -d2);
 
 % equation 22 in old 1981 paper, applied to boundary 1-2
-dz_1 = 1 - d1/h;
+dz_1 = h - d1;
 match_12_potential = C_1n_1(n) * subs(R_1n_1(n),r,a1) == ...
     ( C_1n_2(n) * subs(R_1n_2(n),r,a1) + C_2n_2(n) * subs(R_2n_2(n),r,a1) ) * dz_1 + ...
-    int(subs(phi_p_i2 - phi_p_i1,r,a1) * Z_n_i1(n), z, 0, dz_1);
+    int(subs(phi_p_i2 - phi_p_i1,r,a1) * Z_n_i1(n), z, -h, -d1);
 
 %phi_2_a2 = subs(phi_2, r, a2);
 %phi_e_a2 = subs(phi_e, r, a2);
@@ -115,161 +115,173 @@ syms C_1n_1_const C_1n_2_const C_2n_1_const C_2n_2_const B_k_const [N+1 1] real
 unknowns_const = [C_1n_1_const; C_1n_2_const; C_2n_1_const; C_2n_2_const; B_k_const];
 eqns = subs(eqns, unknowns, unknowns_const');
 
-h_num = 1;
-m_k_num = 1;
-a1_num = .5;
-a2_num = 1;
-d1_num = .5;
-d2_num = .25;
-m0_num = 1;
-params = {h m_k a1 a2 d1 d2 m0};
-params_num = {h_num m_k_num a1_num a2_num d1_num d2_num m0_num};
-eqns = subs(eqns,params,params_num);
+symvar(eqns)
 
-solns = vpasolve(eqns, unknowns_const);
-solns
-
-% build this...
-%C_1n_1_const_list = solns[0:N] 
-%eq6
-%phi_h_n_i1_solns = (solns.C_1n_1_const1 * R_1n_1(0) + solns.C_2n_1_const1 * R_2n_1(0)) * Z_n_i1(0);
-%phi_h_n_i2_solns = (solns.C_1n_2_const2 * R_1n_2(0) + solns.C_2n_2_const2 * R_2n_2(0)) * Z_n_i2(0);
-
-fns = fieldnames(solns);
-length(fns)
-
-C_1n_1s = ones(N+1,1);
-C_2n_1s = ones(N+1,1);
-C_1n_2s = ones(N+1,1);
-C_2n_2s = ones(N+1,1);
-B_ks = ones(N+1,1);
-index = 1;
-
-for k = 1:length(fns)
-    if index > N+1
-        index = 1;
-    end
-   
-    if (k<=N+1)
-        C_1n_1s(index) = solns.(fns{k});
-    end
-    if (N+1<k) && (k<=2*(N + 1))
-        C_1n_2s(index) = solns.(fns{k});
-       
-    end
-    if (k>2*(N+1)) && (k<=3*(N +1) )
-         C_2n_1s(index) = solns.(fns{k});
-         
-    end
-
-    if (k>3*(N+1)) && (k<=4*(N+1))
-         C_2n_2s(index) = solns.(fns{k});
-       
-    end
-
-    if (k>4*(N+1)) 
-         B_ks(index) = solns.(fns{k});
-    end
-    index = index + 1;
-
-
-end
-
-phi_h_n_i1_solns_all = (C_1n_1s' .* R_1n_1(0:N) + C_2n_1s' .* R_2n_1(0:N)) .* Z_n_i1(0:N); 
-phi_h_n_i2_solns_all = (C_1n_2s' .* R_1n_2(0:N) + C_2n_2s' .* R_2n_2(0:N)) .* Z_n_i2(0:N);
-
-phi_e_k = B_ks' .* Lambda_k(0:N) .* Z_k_e(0:N);
-
-
-% summing all to get the potential 
-phi_h_i1 = sum(phi_h_n_i1_solns_all,2);
-phi_h_i2 = sum(phi_h_n_i2_solns_all,2);
-
-phi_e_sym = sum(phi_e_k);
-
-% 36 and 37
-phi_1_sym = phi_h_i1 + phi_p_i1;
-phi_2_sym = phi_h_i2 + phi_p_i2;
-
-phi_1 = subs(phi_1_sym,params,params_num);
-phi_2 = subs(phi_2_sym,params,params_num);
-phi_e = subs(phi_e_sym,params,params_num);
-
-%pretty(phi_1)
-%pretty(phi_2)
-
-res = 30;
-r_vec = linspace(0,2*a2_num,res);
-z_vec = linspace(0,h_num,res);
-[R,Z] = meshgrid(r_vec,z_vec);
-
-phi1 = double(subs(phi_1,{r,z},{R,Z}));
-phi2 = double(subs(phi_2,{r,z},{R,Z}));
-phie = double(subs(phi_e,{r,z},{R,Z}));
-
-regione = R > a2_num;
-region1 = R < a1_num & Z < (h_num - d1_num);
-region2 = R > a1_num & R <= a2_num & Z < (h_num - d2_num);
-
-phi = NaN(size(R));
-phi(region1) = phi1(region1);
-phi(region2) = phi2(region2);
-phi(regione) = phie(regione);
-%%
-figure
-levels = [linspace(0,2,5) 3:9];
-subplot 121
-[c,h_fig] = contourf(R,Z,real(phi),levels);
-clabel(c,h_fig)
-xlabel('R')
-ylabel('Z')
-title('Velocity Potential - Real')
-colorbar
-
-imag_phi = imag(phi);
-imag_phi(~region1 & ~region2 & ~regione) = NaN;
-
-subplot 122
-[c,h_fig] = contourf(R,Z,imag_phi);
-clabel(c,h_fig)
-xlabel('R')
-ylabel('Z')
-title('Velocity Potential - Imaginary')
-colorbar
-
-%% force
-force_2_over_rho_w_eiwt = 1i * int( int(r*phi_2_sym,a1,a2), 0, 2*pi );
-
-syms a1_over_a2 real positive
-force_2_over_rho_w_eiwt = subs(force_2_over_rho_w_eiwt, {h, a1},{1, a1_over_a2 * a2});
-symvar(force_2_over_rho_w_eiwt)
+h_mat = 1;
 
 res=10;
-a2_vec = linspace(.1,2,res);
-d2_vec = linspace(.1,.5,res);
-a1_over_a2_vec = [.25 .5 .75 .9];
+a2_vec = 1;
+d2_vec = .25;
+a1_vec = .5;
+d1_vec = 2;
+m0_vec = 1;
 
-[a2_mat,d2_mat,a1_over_a2_mat] = meshgrid(a2_vec,d2_vec,a1_over_a2_vec);
-%%
-force = real(double(vpa(subs(force_2_over_rho_w_eiwt,{a2,d2,a1_over_a2},{a2_mat,d2_mat,a1_over_a2_mat}))));
+[a2_mat,d2_mat,a1_mat,d1_mat,m0_mat] = ndgrid(a2_vec,d2_vec,a1_vec, d1_vec, m0_vec);
 
-figure
-for plot_num = 1:length(a1_over_a2_vec)
-    subplot(2,2,plot_num)
-    contourf(a2_mat(:,:,plot_num),d2_mat(:,:,plot_num),force(:,:,plot_num))
-    title(["a_1/a_2 = " num2str(a1_over_a2_vec(plot_num))])
-    xlabel('a_2')
-    ylabel('d_2')
+m_k_mat = m0_mat; % todo implement dispersion relation here
+
+[phi, force, A, B] = deal(zeros(size(a2_mat)));
+
+for i=1:length(a2_mat)
+    [phi(i), force(i), A(i), B(i)] = get_phi_force(eqns, unknowns_const, N, ...
+        R_1n_1, R_1n_2, R_2n_1, R_2n_2, Z_n_i1, Z_n_i2, Lambda_k, Z_k_e, ...
+        phi_p_i1, phi_p_i2, r, z, ...
+        h_mat, m_k_mat, a1_mat, a2_mat, d1_mat, d2_mat, m0_mat);
+
+    figure
+    levels = [linspace(0,2,5) 3:9];
+    subplot 121
+    [c,h_fig] = contourf(R,Z,real(phi),levels);
+    clabel(c,h_fig)
+    xlabel('R')
+    ylabel('Z')
+    title('Velocity Potential - Real')
+    colorbar
+    
+    imag_phi = imag(phi);
+    imag_phi(~region1 & ~region2 & ~regione) = NaN;
+    
+    subplot 122
+    [c,h_fig] = contourf(R,Z,imag_phi);
+    clabel(c,h_fig)
+    xlabel('R')
+    ylabel('Z')
+    title('Velocity Potential - Imaginary')
     colorbar
 end
-sgtitle('Heave Exciting Force')
 
-% force2 = NaN(size(R));
-% force2(region2) = force_2_over_rho_w_eiwt(region2);
-% 
+%% multidim plot over geometry
 % figure
-% contourf(R,Z,real(force2))
-% xlabel('R')
-% ylabel('Z')
-% title('Heave Force on float')
-% colorbar
+% for plot_num = 1:length(a1_over_a2_vec)
+%     subplot(2,2,plot_num)
+%     contourf(a2_mat(:,:,plot_num),d2_mat(:,:,plot_num),force(:,:,plot_num))
+%     title(["a_1/a_2 = " num2str(a1_over_a2_vec(plot_num))])
+%     xlabel('a_2')
+%     ylabel('d_2')
+%     colorbar
+% end
+% sgtitle('Heave Exciting Force')
+
+%%
+function [phi, force, A, B] = get_phi_force(eqns, unknowns_const, N, ...
+                R_1n_1, R_1n_2, R_2n_1, R_2n_2, Z_n_i1, Z_n_i2, Lambda_k, Z_k_e, ...
+                phi_p_i1, phi_p_i2, r, z, ...
+                h_num, m_k_num, a1_num, a2_num, d1_num, d2_num, m0_num)
+
+
+    syms h m_k a1 a2 d1 d2 m0 real positive 
+    params = {h m_k a1 a2 d1 d2 m0};
+    params_num = {h_num m_k_num a1_num a2_num d1_num d2_num m0_num};
+    eqns = subs(eqns,params,params_num);
+    
+    solns = vpasolve(eqns, unknowns_const);
+    
+    fns = fieldnames(solns);
+    length(fns)
+    
+    C_1n_1s = ones(N+1,1);
+    C_2n_1s = ones(N+1,1);
+    C_1n_2s = ones(N+1,1);
+    C_2n_2s = ones(N+1,1);
+    B_ks = ones(N+1,1);
+    index = 1;
+    
+    for k = 1:length(fns)
+        if index > N+1
+            index = 1;
+        end
+       
+        if (k<=N+1)
+            C_1n_1s(index) = solns.(fns{k});
+        end
+        if (N+1<k) && (k<=2*(N + 1))
+            C_1n_2s(index) = solns.(fns{k});
+           
+        end
+        if (k>2*(N+1)) && (k<=3*(N +1) )
+             C_2n_1s(index) = solns.(fns{k});
+             
+        end
+    
+        if (k>3*(N+1)) && (k<=4*(N+1))
+             C_2n_2s(index) = solns.(fns{k});
+           
+        end
+    
+        if (k>4*(N+1)) 
+             B_ks(index) = solns.(fns{k});
+        end
+        index = index + 1;
+    
+    
+    end
+    
+    phi_h_n_i1_solns_all = (C_1n_1s' .* R_1n_1(0:N) + C_2n_1s' .* R_2n_1(0:N)) .* Z_n_i1(0:N); 
+    phi_h_n_i2_solns_all = (C_1n_2s' .* R_1n_2(0:N) + C_2n_2s' .* R_2n_2(0:N)) .* Z_n_i2(0:N);
+    
+    phi_e_k = B_ks' .* Lambda_k(0:N) .* Z_k_e(0:N);
+    
+    
+    % summing all to get the potential 
+    phi_h_i1 = sum(phi_h_n_i1_solns_all,2);
+    phi_h_i2 = sum(phi_h_n_i2_solns_all,2);
+    
+    phi_e_sym = sum(phi_e_k);
+    
+    % 36 and 37
+    phi_1_sym = phi_h_i1 + phi_p_i1;
+    phi_2_sym = phi_h_i2 + phi_p_i2;
+    
+    phi_1 = subs(phi_1_sym,params,params_num);
+    phi_2 = subs(phi_2_sym,params,params_num);
+    phi_e = subs(phi_e_sym,params,params_num);
+    
+    %pretty(phi_1)
+    %pretty(phi_2)
+    
+    res = 30;
+    r_vec = linspace(0,2*a2_num,res);
+    z_vec = linspace(0,h_num,res);
+    [R,Z] = meshgrid(r_vec,z_vec);
+    
+    phi1 = double(subs(phi_1,{r,z},{R,Z}));
+    phi2 = double(subs(phi_2,{r,z},{R,Z}));
+    phie = double(subs(phi_e,{r,z},{R,Z}));
+    
+    regione = R > a2_num;
+    region1 = R < a1_num & Z < (h_num - d1_num);
+    region2 = R > a1_num & R <= a2_num & Z < (h_num - d2_num);
+    
+    phi = NaN(size(R));
+    phi(region1) = phi1(region1);
+    phi(region2) = phi2(region2);
+    phi(regione) = phie(regione);
+
+    force_2_over_rho_w_eiwt = 1i * int( int(r*phi_2_sym,a1,a2), 0, 2*pi );
+    
+    syms a1_over_a2 real positive
+    force_2_over_rho_w_eiwt = subs(force_2_over_rho_w_eiwt, {h, a1},{1, a1_over_a2 * a2});
+    symvar(force_2_over_rho_w_eiwt)
+
+    force = real(double(vpa(subs(force_2_over_rho_w_eiwt,{a2,d2,a1_over_a2},{a2_num,d2_num,a1_over_a2_num}))));
+
+    k = m0_num;
+    g = 9.8;
+    rho = 1025;
+    omega = sqrt(k*g);
+    X = force * rho * omega;
+    Vg = g/(2*omega);
+    B = k/ (4*rho*g*Vg) * X.^2;
+
+    A = B; % todo implement kramers kronig relationship
+end
