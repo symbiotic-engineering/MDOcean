b6646f5391ecf6f58b9769638529f25f2e76293f
Rebecca McCabe <chessgirl2400@gmail.com>
2022-12-14 19:24:30 -0500

    fix loop running and add dispersion relation m_k equation

--- dev/Chau_Yeung_2010.m
+++ dev/Chau_Yeung_2010.m
@@ -2,11 +2,12 @@ clear all
 close all
 
 N = 20;
-syms r theta z real
-syms R_1n_1(n) R_1n_2(n) R_2n_2(n) Z_n_i1(n) Z_n_i2(n) Lambda_k(k) N_k(k) Z_k_e(k)
-syms h m_k a1 a2 d1 d2 m0 real positive
-syms n k real positive
-syms C_1n_1(n) C_1n_2(n) C_2n_1(n) C_2n_2(n) B_k(k) %[N+1 1]
+syms r theta z real % coordinates
+syms R_1n_1(n) R_1n_2(n) R_2n_2(n) Z_n_i1(n) Z_n_i2(n) ...
+        Lambda_k(k) N_k(k) Z_k_e(k) m_k(k) % expressions in basis functions
+syms h a1 a2 d1 d2 m0 real positive % constants
+syms n k real positive % counter indices
+syms C_1n_1(n) C_1n_2(n) C_2n_1(n) C_2n_2(n) B_k(k) % unknown coefficients
 
 % eq 4
 lambda_n1(n) = n*pi/(h-d1);
@@ -41,6 +42,9 @@ phi_h_n_i2(n) = (C_1n_2(n) * R_1n_2(n)+ C_2n_2(n) * R_2n_2(n)) * Z_n_i2(n);
 
 % pretty(phi_1)
 % pretty(phi_2)
+
+% eq 11
+m_k(k) = -m0 * tanh(m0*h) / tan(m_k(k) * h);
 
 % eq 13
 Lambda_k(k) = piecewise(k==0, besselh(0,1,m0*r)/besselh(0,1,m0*a2), k>=1, besselk(0,m_k*r)/besselk(0,m_k*a2));
@@ -119,8 +123,9 @@ symvar(eqns)
 
 h_mat = 1;
 
-res=10;
-a2_vec = 1;
+spatial_res = 30;
+sweep_res = 10;
+a2_vec = 1; % linspace(.5, 1, sweep_res);
 d2_vec = .25;
 a1_vec = .5;
 d1_vec = 2;
@@ -130,34 +135,18 @@ m0_vec = 1;
 
 m_k_mat = m0_mat; % todo implement dispersion relation here
 
-[phi, force, A, B] = deal(zeros(size(a2_mat)));
+phi = zeros(spatial_res,spatial_res,numel(a2_mat));
+[force, A, B] = deal(zeros(size(a2_mat)));
 
-for i=1:length(a2_mat)
-    [phi(i), force(i), A(i), B(i)] = get_phi_force(eqns, unknowns_const, N, ...
+plot_phi = true;
+
+for i=1:numel(a2_mat)
+    disp(['running ' num2str(i) ' out of ' num2str(length(a2_mat))]);
+    [phi(:,:,i), force(i), A(i), B(i)] = get_phi_force(eqns, unknowns_const, N, ...
         R_1n_1, R_1n_2, R_2n_1, R_2n_2, Z_n_i1, Z_n_i2, Lambda_k, Z_k_e, ...
-        phi_p_i1, phi_p_i2, r, z, ...
+        phi_p_i1, phi_p_i2, r, z, spatial_res, plot_phi, ...
         h_mat, m_k_mat, a1_mat, a2_mat, d1_mat, d2_mat, m0_mat);
 
-    figure
-    levels = [linspace(0,2,5) 3:9];
-    subplot 121
-    [c,h_fig] = contourf(R,Z,real(phi),levels);
-    clabel(c,h_fig)
-    xlabel('R')
-    ylabel('Z')
-    title('Velocity Potential - Real')
-    colorbar
-    
-    imag_phi = imag(phi);
-    imag_phi(~region1 & ~region2 & ~regione) = NaN;
-    
-    subplot 122
-    [c,h_fig] = contourf(R,Z,imag_phi);
-    clabel(c,h_fig)
-    xlabel('R')
-    ylabel('Z')
-    title('Velocity Potential - Imaginary')
-    colorbar
 end
 
 %% multidim plot over geometry
@@ -175,9 +164,8 @@ end
 %%
 function [phi, force, A, B] = get_phi_force(eqns, unknowns_const, N, ...
                 R_1n_1, R_1n_2, R_2n_1, R_2n_2, Z_n_i1, Z_n_i2, Lambda_k, Z_k_e, ...
-                phi_p_i1, phi_p_i2, r, z, ...
+                phi_p_i1, phi_p_i2, r, z, spatial_res, plot_phi, ...
                 h_num, m_k_num, a1_num, a2_num, d1_num, d2_num, m0_num)
-
 
     syms h m_k a1 a2 d1 d2 m0 real positive 
     params = {h m_k a1 a2 d1 d2 m0};
@@ -187,7 +175,6 @@ function [phi, force, A, B] = get_phi_force(eqns, unknowns_const, N, ...
     solns = vpasolve(eqns, unknowns_const);
     
     fns = fieldnames(solns);
-    length(fns)
     
     C_1n_1s = ones(N+1,1);
     C_2n_1s = ones(N+1,1);
@@ -249,9 +236,8 @@ function [phi, force, A, B] = get_phi_force(eqns, unknowns_const, N, ...
     %pretty(phi_1)
     %pretty(phi_2)
     
-    res = 30;
-    r_vec = linspace(0,2*a2_num,res);
-    z_vec = linspace(0,h_num,res);
+    r_vec = linspace(0,2*a2_num,spatial_res);
+    z_vec = linspace(0,h_num,spatial_res);
     [R,Z] = meshgrid(r_vec,z_vec);
     
     phi1 = double(subs(phi_1,{r,z},{R,Z}));
@@ -267,13 +253,34 @@ function [phi, force, A, B] = get_phi_force(eqns, unknowns_const, N, ...
     phi(region2) = phi2(region2);
     phi(regione) = phie(regione);
 
+    if plot_phi
+        figure
+        levels = [linspace(0,2,5) 5:5:30];
+        subplot 121
+        [c,h_fig] = contourf(R,Z,real(phi),levels);
+        clabel(c,h_fig)
+        xlabel('R')
+        ylabel('Z')
+        title('Velocity Potential - Real')
+        colorbar
+        
+        imag_phi = imag(phi);
+        imag_phi(~region1 & ~region2 & ~regione) = NaN;
+        
+        subplot 122
+        [c,h_fig] = contourf(R,Z,imag_phi);
+        clabel(c,h_fig)
+        xlabel('R')
+        ylabel('Z')
+        title('Velocity Potential - Imaginary')
+        colorbar
+    end
+
     force_2_over_rho_w_eiwt = 1i * int( int(r*phi_2_sym,a1,a2), 0, 2*pi );
     
-    syms a1_over_a2 real positive
-    force_2_over_rho_w_eiwt = subs(force_2_over_rho_w_eiwt, {h, a1},{1, a1_over_a2 * a2});
     symvar(force_2_over_rho_w_eiwt)
 
-    force = real(double(vpa(subs(force_2_over_rho_w_eiwt,{a2,d2,a1_over_a2},{a2_num,d2_num,a1_over_a2_num}))));
+    force = real(double(vpa(subs(force_2_over_rho_w_eiwt,{h,a1,a2,d2},{h_num,a1_num,a2_num,d2_num}))));
 
     k = m0_num;
     g = 9.8;
