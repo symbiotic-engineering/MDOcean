ab0aeda68396e0eb359e683c1b159af25ce5ba38
rebeccamccabe <chessgirl2400@gmail.com>
2024-05-15 14:48:55 -0400

    fix design of experiments

--- dev/experiments.m
+++ dev/experiments.m
@@ -1,62 +1,77 @@
+
+% Runs one-at-a-time design of experiments
+
 clear;clc;close all
 
 p = parameters();
 b = var_bounds();
 
-% X = [ 20 10 30;     % outer diameter of float	
-%       .3 .1 .5;     % inner diameter ratio of float	
-%       1.5 1.2 2;     % outer diameter of reaction plate	
-%       1 2 3;        % material	
-%       10 1 20;      % Number of WECs in array	
-%       10 5 50       % D_int	
-%       2*pi/7 2*pi/8 2*pi/9
-%         ];	
-
+DOE_strategy = 'ratios'; % 'sample' or 'bounds' or 'ratios'
 n = 20;
-% X = [ b.D_f_nom, linspace(b.D_f_min, b.D_f_max, n);
-%       b.D_s_ratio_nom, linspace(b.D_s_ratio_min, b.D_s_ratio_max, n);
-%       b.D_or_ratio_nom, linspace(b.D_or_ratio_min, b.D_or_ratio_max, n);
-%       1, ones([1 n]);
-%       b.F_max_nom, linspace(b.F_max_min, b.F_max_max, n);
-%       b.D_int_nom, linspace(b.D_int_min, b.D_int_max, n);
-%       b.w_n_nom, linspace(b.w_n_min, b.w_n_max, n) ];
-%   ratios = X./X(:,1);
 
-ratios = logspace(log10(1/3),log10(3),n);
-ratios = [1, ratios(ratios~=1)];
-%X =  [b.D_f_nom, b.D_s_ratio_nom, b.D_or_ratio_nom, 1, b.F_max_nom, b.B_p_nom, b.w_n_nom]' * ratios;
-X =  [b.D_f_nom b.D_s_ratio_nom b.h_f_ratio_nom b.T_s_ratio_nom b.F_max_nom b.B_p_nom b.w_n_nom 1]' * ratios;
-X(8,:) = ones(1,n+1);
+if strcmp(DOE_strategy,'sample')
+    X = [ 20 10 30;     % outer diameter of float	
+          .3 .1 .5;     % inner diameter ratio of float	
+          1.5 1.2 2;    % outer diameter of reaction plate	
+          1 2 3;        % material	
+          10 1 20;      % Number of WECs in array	
+          10 5 50       % D_int	
+          2*pi/7 2*pi/8 2*pi/9
+        ];	
+elseif strcmp(DOE_strategy,'bounds')
+    X = [ b.D_f_nom, linspace(b.D_f_min, b.D_f_max, n);
+          b.D_s_ratio_nom, linspace(b.D_s_ratio_min, b.D_s_ratio_max, n);
+          b.h_f_ratio_nom, linspace(b.h_f_ratio_min, b.h_f_ratio_max, n);
+          b.T_s_ratio_nom, linspace(b.T_s_ratio_min, b.T_s_ratio_max, n);
+          b.F_max_nom, linspace(b.F_max_min, b.F_max_max, n);
+          b.B_p_nom, linspace(b.B_p_min, b.B_p_max, n);
+          b.w_n_nom, linspace(b.w_n_min, b.w_n_max, n) ];
+    ratios = X./X(:,1);
+elseif strcmp(DOE_strategy,'ratios')
+    ratios = logspace(log10(1/3),log10(3),n);
+    ratios = [1, ratios(ratios~=1)];
+    X =  [b.D_f_nom b.D_s_ratio_nom b.h_f_ratio_nom b.T_s_ratio_nom b.F_max_nom b.B_p_nom b.w_n_nom b.M_nom]' * ratios;
+end
 
 X_nom = X(:,1);	
 design_size = size(X);	
-var_num = design_size(1);	
-var_depth = design_size(2);	
+num_vars = design_size(1);	
+num_vals_per_var = design_size(2);
+num_vals_swept = num_vals_per_var - 1; % don't sweep 1st value of each variable (corresponding to ratio 1)
+
+% don't sweep material
+idx_material = 8;
+X(idx_material,:) = b.M_nom * ones(1,n+1);
+num_vars_swept = num_vars - 1;
+
+% initialize variables
 LCOE = X*inf;
 P_var = X*inf;
-opt_idx = zeros(var_num,1);	
-recommended = zeros(var_num,2);	
-number_runs = var_depth + (var_num-1)*(var_depth-1);	
+opt_idx = zeros(num_vars,1);	
+recommended = zeros(num_vars,2);	
+number_runs = 1 + num_vars_swept * num_vals_swept; % nominal run plus all sweeps
 failed = cell(number_runs,1);	
 power = zeros(number_runs,1);	
 FOS = zeros(number_runs,1);	
-X_ins = zeros(number_runs, var_num);	
-design = 0;	
-for i = 1:var_num-1	
+X_ins = zeros(number_runs, num_vars);	
+design = 0;
+
+% run design of experiments
+for i = 1:num_vars_swept
     X_in = X_nom;	
-    for j = 1:var_depth-1	
-        if i == 1 || j~=1	
+    for j = 1:num_vals_per_var
+        if i == 1 || j~=1	% prevent rerunning nominal multiple times
             changed_entry = X(i,j);	
             if ~isnan(changed_entry)	
                 design = design+1;	
                 X_in(i) = changed_entry;	
                 X_ins(design,:) = X_in;	
-                [LCOE_temp, P_var_temp, P_matrix, g, val] = simulation(X_in,p);	
-                disp(g)
-                FOS(design) = min([(g(6)+1)*p.FOS_min,(g(7)+1)*p.FOS_min,(g(8)+1)*p.FOS_min,(g(9)+1)*p.FOS_min]);
+                [LCOE_temp, P_var_temp, P_matrix, g, val] = simulation(X_in,p);
+
+                % only add to results if first 12 constraints are feasible
                 g(13) = 0;
                 g(14) = 0;
-                [feasible, failed] = is_feasible(g, b);
+                [feasible, which_failed] = is_feasible(g, b);
                 if feasible	
                     LCOE(i,j) = LCOE_temp;	
                     P_var(i,j) = P_var_temp;
@@ -64,47 +79,32 @@ for i = 1:var_num-1
                     LCOE(i,j) = NaN;
                     P_var(i,j) = NaN;
                 end	
+                failed{design} = which_failed;
             end	
         end	
     end	
     [~, opt_idx(i)] = min(LCOE(i,:));	
     recommended(i,:) = [X(i,opt_idx(i)), opt_idx(i)];	
 end	
-%[LCOE, P_var, P_matrix, g, val] = simulation(X,p);	
-%[feasible, failed] = is_feasible(g, b);	
 
 % create table for display	
-var_names = {'D_f',...    % outer diameter of float (m)	
-            'D_s/D_f',... % inner diameter ratio of float (m)	
-            'D_or/D_f',...      % outer diameter ratio of reaction plate (m)	
-            'M',...         % material (-)	
-            'F_max',...     % number of WECs in array (-)	
-            'B_p',...     % internal damping of controller (Ns/m)	
-            'w_n'};         % natural frequency (rad/s)
-var_names_pretty = {
-            'D_{f}',...    % outer diameter of float (m)	
-            'D_s/D_{f}',... % inner diameter ratio of float (m)	
-            'D_{or}/D_{f}',...      % outer diameter of reaction plate (m)	
-            'M',...         % material (-)	
-            'F_{max}',...     % number of WECs in array (-)	
-            'D_{int}',...     % internal damping of controller (Ns/m)	
-            '\omega_n'};         % natural frequency (rad/s)
-%results = array2table(X_ins, 'VariableNames', var_names);	
+results = array2table(X_ins, 'VariableNames', b.var_names);	
 LCOE = LCOE';
 P_var = P_var';
-%results = addvars(results, round(LCOE(LCOE~=Inf),1), round(P_var(P_var~=Inf)), failed, ...	
-    %'NewVariableNames', {'LCOE ($/kWh)','c_v (%)','ConstraintsFailed'});	
-%disp(results)
+results = addvars(results, round(LCOE(LCOE~=Inf),2), round(P_var(P_var~=Inf),1), failed, ...
+                'NewVariableNames', {'LCOE ($/kWh)','c_v (%)','Failed Constraints'});	
+disp(results)
 
-
+% plot pareto curve for comparison
 pareto_curve_heuristics()
 figure(2)
 plot(LCOE, P_var, '*--')
 xlabel('LCOE')
 ylabel('P_{var}')
 title('Design of Experiments Pareto Front')
-legend(var_names_pretty)
+l = legend(b.var_names_pretty);
 improvePlot
+l.Location = 'bestoutside';
 
 %% sensitivities plot
 [ratios_sorted,idx] = sort(ratios);
@@ -125,15 +125,12 @@ ylabel('Power c_v (%)')
 grid on
 
 title(t,'Design of Experiments Results','FontWeight','bold','FontSize',20)
-l = legend(var_names_pretty);
+l = legend(b.var_names_pretty);
 l.Location = 'bestoutside';
-%l.Position = [.75 .5 .15 .3];
 grid on
 linkaxes([ax1,ax2],'x');
 xlabel('Design Variable Ratio (-)')
 xticklabels(ax1,{})
 xticks(ax2,xticks(ax1))
 improvePlot
-
-%pareto_curve_heuristics(figure(5))
 
