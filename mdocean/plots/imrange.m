f44381bf57cda6cb74601de08e41eed8a3b9d742
rebeccamccabe <chessgirl2400@gmail.com>
2024-05-21 20:44:12 -0400

    add attribution to imrange

--- mdocean/plots/imrange.m
+++ mdocean/plots/imrange.m
@@ -1,8 +1,10 @@
-function [minval maxval] = imrange(inpict)
+function [minval, maxval] = imrange(inpict)
 %   [min max] = IMRANGE(INPICT)
 %       returns the global min and max pixel values in INPICT
 %       
 %   INPICT can be a vector or array of any dimension or class
+
+% downloaded from https://www.mathworks.com/matlabcentral/answers/1700655-symmetric-diverging-log-color-scale#answer_1380451
 
 x = inpict(:);
 minval = double(min(x));
