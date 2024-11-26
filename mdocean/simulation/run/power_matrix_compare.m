7d4e783c38547769db4728b36956641d91b74108
rebeccamccabe <chessgirl2400@gmail.com>
2024-11-26 03:54:23 -0500

    move power matrix compare from dev to sim folder

--- mdocean/simulation/run/power_matrix_compare.m
+++ mdocean/simulation/run/power_matrix_compare.m
@@ -59,30 +59,6 @@ end
 
 %%
 function old_stuff()
-% wave resources
-[T,H] = meshgrid(p.T, p.Hs);
-JPD = p.JPD;
-
-% variable manipulation before plotting
-power_titles = {'RM3 Report','MDOcean',...
-        'WecSim spar moving','WecSim spar fixed'};
-    %'Actual: Saturated','Simulated: Saturated'};
-pre_JPD_power        = actual_mech_unsat;
-pre_JPD_power(:,:,2) = sim_mech_unsat;
-%pre_JPD_power(:,:,3) = actual_elec_sat;
-%pre_JPD_power(:,:,4) = sim_elec_sat;
-pre_JPD_power(:,:,3) = spar_moving_power;
-pre_JPD_power(:,:,4) = spar_fixed_power;
-
-post_JPD_power = pre_JPD_power .* JPD / 100;
-
-vars = pre_JPD_power;
-%vars(:,:,:,2) = post_JPD_power;
-vars(:,:,:,2) = CW;
-% vars(:,:,:,4) = CWR;
-vars(:,:,:,3) = CW_to_CW_max;
-% vars(:,:,:,6) = CW_to_CW_max_zeroed;
-vars(:,:,:,4) = pre_JPD_power ./ H.^2;
 
 var_names = {'Unweighted Device Power Matrix (kW)',...
 ...%     'JPD-Weighted Power Matrix (kW)',...
@@ -91,64 +67,6 @@ var_names = {'Unweighted Device Power Matrix (kW)',...
      'Capture Width / Max Capture Width (-)',...
 ...%     'Capture Width / Max Capture Width (-)',...
     'Unweighted Device Power Matrix per H^2 (kW/m^2)'};
-
-% four figures showing weigted and unweighted power and CWR
-% for fig = 1:size(vars,4)
-%     var = vars(:,:,:,fig);
-%     figure
-%     for i = 1:4
-%         subplot(2,2,i)
-%         contourf(T,H,var(:,:,i))
-%         xlabel('T (s)')
-%         ylabel('Hs (m)')
-%         title(power_titles{i})
-%         colorbar
-%     end
-%     sgtitle(var_names{fig})
-% 
-% end
-% 
-% % % resource figure
-% resource_titles = {'Raw Resource','Zeroed Resource (Actual)','Zeroed Resource (Sim)'};
-% wave_resources = wave_resource_raw;
-% wave_resources(:,:,2) = wave_resource_sheet;
-% wave_resources(:,:,3) = wave_resource_sim;
-% figure
-% for i = 1:3
-%     subplot(1,3,i)
-%     contourf(T,H,wave_resources(:,:,i))
-%     xlabel('Wave Period T (s)')
-%     ylabel('Wave Height Hs (m)')
-%     title(resource_titles{i})
-%     colorbar
-% end
-% sgtitle('Wave Resource (kW/m)')
-% 
-% % JPD figure
-% JPDs = JPD;
-% JPDs(:,:,2) = JPD_actual/100;
-% JPD_titles = {'Sim','Actual'};
-% figure
-% for i = 1:2
-%     subplot(1,2,i)
-%     contourf(T,H,JPDs(:,:,i))
-%     xlabel('Wave Period T (s)')
-%     ylabel('Wave Height Hs (m)')
-%     colorbar
-%     grid on
-%     title(JPD_titles{i})
-% end
-% sgtitle('JPD (-)')
-% power error plot: vs RM3 report
-% figure                      % (sim - actual) / actual
-% 
-% contourf(T,H,pre_JPD_pct_error,-10:10:100)
-% xlabel('Wave Period T (s)')
-% ylabel('Wave Height Hs (m)')
-% title('Unsaturated Power Percent Error to RM Report (%)')
-% colorbar
-% grid on
-% power and amplitude error plot: vs WecSim
 
 % set contour lines to make plot more readable
 if p.C_d_float==0 && p.use_MEEM==false
@@ -167,29 +85,7 @@ end
 min_colorbar = min(min([MDOcean_Wecsim_power_error,MDOcean_Wecsim_amplitude_error],[],'all'),-5);
 max_colorbar = max(max([MDOcean_Wecsim_power_error,MDOcean_Wecsim_amplitude_error],[],'all'),5);
 
-% figure
-% subplot 121
-% [c,h_fig] = contourf(T,H,MDOcean_Wecsim_power_error,power_error_vals);
-% clabel(c,h_fig);
-% xlabel('Wave Period T (s)')
-% ylabel('Wave Height Hs (m)')
-% title('Unsaturated Power')
-% colorbar
-% clim([min_colorbar max_colorbar])
-% colormap(bluewhitered)
-% grid on
-% subplot 122
-% [c,h_fig] = contourf(T,H,MDOcean_Wecsim_amplitude_error,X_error_vals);
-% clabel(c,h_fig);
-% xlabel('Wave Period T (s)')
-% ylabel('Wave Height Hs (m)')
-% title('Unsaturated Amplitude')
-% colorbar
-% clim([min_colorbar max_colorbar])
-% colormap(bluewhitered)
-% grid on
-% sgtitle('Percent Error to WecSim (%)')
-% 
+
 % % Bp (without power saturation)
 % if length(unique(val.B_p)) > 1
 %     figure
@@ -200,62 +96,6 @@ max_colorbar = max(max([MDOcean_Wecsim_power_error,MDOcean_Wecsim_amplitude_erro
 %     colorbar
 %     grid on
 % end
-% 
-% % X_f float amplitude
-% figure
-% subplot 131
-% contourf(T,H,val.X_f)
-% title('MDOcean')
-% xlabel('Wave Period T (s)')
-% ylabel('Wave Height Hs (m)')
-% colorbar
-% grid on
-% 
-% subplot 132
-% contourf(T,H,spar_fixed_float_amplitude)
-% title('WEC-Sim spar fixed')
-% xlabel('Wave Period T (s)')
-% ylabel('Wave Height Hs (m)')
-% colorbar
-% grid on
-% 
-% subplot 133
-% contourf(T,H,spar_moving_float_amplitude)
-% xlabel('Wave Period T (s)')
-% ylabel('Wave Height Hs (m)')
-% title('WEC-Sim spar moving')
-% colorbar
-% grid on
-% 
-% sgtitle('Float Amplitude (m)')
-% 
-% % X_u relative (PTO) amplitude
-% figure
-% subplot 131
-% contourf(T,H,val.X_u)
-% title('MDOcean')
-% xlabel('Wave Period T (s)')
-% ylabel('Wave Height Hs (m)')
-% colorbar
-% grid on
-% 
-% subplot 132
-% contourf(T,H,spar_fixed_float_amplitude) % X_f = X_u when spar fixed
-% title('WEC-Sim spar fixed')
-% xlabel('Wave Period T (s)')
-% ylabel('Wave Height Hs (m)')
-% colorbar
-% grid on
-% 
-% subplot 133
-% contourf(T,H,spar_moving_relative_amplitude,0:.2:1.6)
-% xlabel('Wave Period T (s)')
-% ylabel('Wave Height Hs (m)')
-% title('WEC-Sim spar moving')
-% colorbar
-% grid on
-% 
-% sgtitle('Relative Amplitude (m)')
 
 end
 
