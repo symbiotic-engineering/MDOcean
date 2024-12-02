6cc669959cd8be508143e0d2c7531b55b71a1002
rebeccamccabe <chessgirl2400@gmail.com>
2024-12-01 23:40:16 -0500

    fix hardcoding of number of DVs in x0 sweep ;

--- mdocean/optimization/sensitivities/random_x0.m
+++ mdocean/optimization/sensitivities/random_x0.m
@@ -3,10 +3,10 @@ function [x0,x0_struct] = random_x0(b)
 mins = b.X_mins;
 maxs = b.X_maxs;
 
-r = rand(7,1);
+r = rand(length(mins),1);
 
 x0 = (maxs-mins).*r + mins;
-x0(8) = randi(b.M_max);
+x0(length(b.var_names)) = randi(b.M_max);
 
 x0_struct = cell2struct(num2cell(x0(1:end-1)),b.var_names(1:end-1));
 
