2318e2ba5f996dc443c1254f8fa7ce99bc6811a8
rebeccamccabe <chessgirl2400@gmail.com>
2024-12-02 03:49:28 -0500

    add nominal to DV table

--- mdocean/optimization/gradient_optim.m
+++ mdocean/optimization/gradient_optim.m
@@ -146,11 +146,11 @@ function [Xs_opt, objs_opt, flags, probs] = optimize_both_objectives(X,p,b,x0_in
         end
     end
     if ploton
-        table_data = [Xs_opt(1:end-1,:), b.X_mins, b.X_maxs];
+        table_data = [Xs_opt(1:end-1,:), b.X_mins, b.X_maxs b.X_noms];
         objs_opt
         flags
         array2table(table_data,'RowNames',b.var_names(1:end-1),...
-                'VariableNames',{'Min LCOE','Min cv','Min bound','Max bound'})
+                'VariableNames',{'Min LCOE','Min cv','Min bound','Max bound','Nom'})
     end
 
 end
