1f3a7c96f96af83b49cab0a43c7c04abffdfa6d5
rebeccamccabe <chessgirl2400@gmail.com>
2025-01-03 03:13:51 -0500

    dispersion relation calc

--- mdocean/simulation/modules/dynamics/get_dynamic_coeffs.m
+++ mdocean/simulation/modules/dynamics/get_dynamic_coeffs.m
@@ -7,8 +7,8 @@ function [m_f,B_h_f,K_h_f,F_f_mag,F_f_phase,...
                                                 C_d_float, C_d_spar, ...
                                                 rho_w, g, ...
                                                 use_MEEM, harmonics, hydro)
-    w = 2*pi./T;        % angular frequency
-    k = w.^2 / g;       % wave number (dispersion relation for deep water)
+    w = 2*pi./T;            % angular frequency
+    k = dispersion(w,g,h);  % wave number (dispersion relation for finite depth water)
 
     a2 = D_f / 2;        % radius
     a1 = D_s / 2;
@@ -64,4 +64,30 @@ function [m_f,B_h_f,K_h_f,F_f_mag,F_f_phase,...
     drag_const_s = 4/(3*pi) * rho_w * A_drag_s * C_d_spar;
     mag_v0_f = H/2 * g .* k ./ w .* exp(-k * T_f/2); % finite depth velocity of incident wave
     mag_v0_s = H/2 * g .* k ./ w .* exp(-k * T_s/2);
+end
+
+function k = dispersion(w,h,g)
+    h_lambda_deep = 0.4; % h/lambda > threshold for deep
+    h_lambda_shallow = 0.05; % h/lambda < threshold for shallow
+    idx_deep = w > sqrt(h_lambda_deep * 2*pi * g / h); 
+    idx_shallow = w < h_lambda_shallow * 2*pi * sqrt(g/h);
+    idx_mid = ~idx_deep & ~idx_shallow;
+
+    k = zeros(size(w));
+    k(idx_deep) = w(idx_deep).^2 / g;
+    k(idx_shallow) = w(idx_shallow) / sqrt(g*h);
+
+    if any(idx_mid,'all')
+        err = 1;
+        iters = 0;
+        k_deep_guess = w(idx_mid).^2 / g;
+        k_guess = k_deep_guess;
+        while err > 0.2 && iters < 50
+            k_new = w(idx_mid).^2 / g ./ tanh(k_guess*h);
+            err = max(abs(k_new - k_guess)./k_deep_guess,[],'all');
+            k_guess = k_new;
+            iters = iters + 1;
+        end
+        k(idx_mid) = k_new;
+    end
 end
