c3e6b7593686fa26111fa6193d37bb384ea54042
rebeccamccabe <chessgirl2400@gmail.com>
2025-01-26 22:02:12 -0500

    PlotFcn fix now works even when PlotFcn is empty

--- mdocean/optimization/run_solver.m
+++ mdocean/optimization/run_solver.m
@@ -28,7 +28,9 @@ function [X_opt,obj_opt,flag,output,lambda,grad,hess,problem] = run_solver(prob,
             problem_s = problem;
             problem_s.options.MaxIterations = 150;
             problem_s.options.MaxFunctionEvaluations = 2000;
-            problem_s.options.PlotFcn{2} = @(x,in1,in2) problem.options.PlotFcn{2}(x .* scale,in1,in2);  
+            if ~isempty(problem.options.PlotFcn)
+                problem_s.options.PlotFcn{2} = @(x,in1,in2) problem.options.PlotFcn{2}(x .* scale,in1,in2);  
+            end
             problem_s.objective = @(x) problem.objective(x .* scale);  
             problem_s.nonlcon   = @(x) problem.nonlcon(x .* scale);
             problem_s.Aineq = problem.Aineq .* scale';
