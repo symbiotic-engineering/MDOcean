0c02f98d300f2cf6d660621ee6b6824c4e80c9e8
rebeccamccabe <chessgirl2400@gmail.com>
2025-02-03 01:04:16 -0500

    fix compare plots, make output table with val, fix legend color error in geometry viz

--- mdocean/plots/compare.m
+++ mdocean/plots/compare.m
@@ -1,67 +1,85 @@
-function [DV_table] = compare(filename_uuid)
+function [DV_table,out_table] = compare(filename_uuid)
+
+if nargin==0
+    filename_uuid = '';
+end
 
 p = parameters();
 b = var_bounds();
 b.filename_uuid = filename_uuid;
 x0_input = b.X_start_struct;
 
-Xs_opt = gradient_optim(x0_input,p,b);
-cheapest = Xs_opt(:,1);
-minVariation = Xs_opt(:,2);
+[Xs_opt,~,~,~,vals_opt] = gradient_optim(x0_input,p,b);
+X_minLCOE = Xs_opt(:,1);
+X_minCapex = Xs_opt(:,2);
+val_minLCOE = vals_opt(1);
+val_minCapex = vals_opt(2);
+
+[X_maxPower,val_maxPower] = max_avg_power(p,b);
 
 p.LCOE_max = 0.1;
-balanced = gradient_optim(x0_input,p,b,2);
+[X_balanced,~,~,~,val_balanced] = gradient_optim(x0_input,p,b,2);
 
-nominal	= [b.X_noms' 1];
+X_nom	= [b.X_noms' 1];
+[~,~,~,~,val_nom] = simulation(X_nom,p);
+
 %%
-X = [nominal;cheapest';minVariation';balanced'];
+X    = [X_nom;   X_minLCOE';  X_minCapex';  X_maxPower';  X_balanced'];
+vals = [val_nom, val_minLCOE, val_minCapex, val_maxPower, val_balanced];
 
-col = {'k','b','r','g'};
+titles = {'Nominal','Min LCOE','Min Capex','Max Power','Balanced'};
+color = {'k','b','r','g','m'};
+num_designs = length(titles);
 
 %% geometry comparison
 figure
-for i=1:4
+for i=1:num_designs
     x = X(i,:);
     hold on
-    visualize_geometry(x,p,false,col{i})
+    visualize_geometry(x,p,false,color{i})
 end
-legend('Nominal','Min LCOE','Min Variation','Balanced')
+legend(titles)
 
 %% power probability comparison
 figure
 t = tiledlayout(2,1);
 t.TileSpacing = 'compact';
 ylabel(t,'Probability (-)','FontWeight','bold')
-for i=1:4
+for i=1:num_designs
     x = X(i,:);
     hold on
-    t = power_PDF(x,p,col{i},t);
+    t = power_PDF(x,p,color{i},t);
 end
-legend('Nominal','Min LCOE','Min Variation','Balanced','location','best')
+legend(titles{:},'location','best')
 
 %% power matrix comparison
-titles = {'Nominal','Min LCOE','Min Variation','Balanced'};
+
 figure
-for i=1:4
+for i=1:num_designs
     x = X(i,:);   
     [~,~,P_matrix] = simulation(x, p);
     P_matrix = P_matrix / 1e3; % convert W to kW
     [T,Hs] = meshgrid(p.T,p.Hs); 
     
-    subplot(2,2,i);
+    subplot(2,3,i);
     hold on
-    contourf(T,Hs,P_matrix .* p.JPD/100,[0 5 10:10:60])
+    contourf(T,Hs,P_matrix .* p.JPD/100)
     improvePlot
     grid on
     title(titles{i})
-    caxis([0 60])
+    caxis([0 15])
     ax = gca;
-    if i==2 || i==4
+    second_col = i==2 || i==5;
+    third_col = i==3;
+    if second_col
         xoffset = -ax.Position(3)*.25;
+    elseif third_col
+        xoffset = -ax.Position(3)*.5;
     else
         xoffset = 0;
     end
-    if i==3 || i==4
+    second_row = i>3;
+    if second_row
         yoffset = ax.Position(4)*.2;
     else
         yoffset = 0;
@@ -79,6 +97,13 @@ text(-30,5,'Wave Height Hs (m)','FontWeight','bold','FontSize',16,'Rotation',90)
 
 %% design variable table
 DV_table = array2table(X.', ...
-        'VariableNames',titles, 'RowNames', b.var_names);
+        'VariableNames',titles, 'RowNames', b.var_names_pretty);
+
+%% output table
+temp_table = struct2table(vals,'RowNames',titles);
+scalar_vals = varfun(@isnumeric, temp_table, 'OutputFormat', 'uniform');
+out_table = rows2vars(temp_table(:,scalar_vals));
+out_table.Properties.RowNames = out_table.OriginalVariableNames;
+out_table = removevars(out_table,'OriginalVariableNames');
 
 end
