432233ce093b6baffdb9e7104c71b7ff98e52bb6
Madison Dietrich <mjd429@cornell.edu>
2025-02-20 13:24:57 -0500

    get rid of white space and overlaps in power matrix plots

--- mdocean/plots/plot_power_matrix.m
+++ mdocean/plots/plot_power_matrix.m
@@ -5,57 +5,61 @@ function plot_power_matrix(X,p,filename_uuid)
 P_matrix = P_matrix / 1e3; % convert W to kW
 [CW_over_CW_max, P_wave, CW_max] = check_max_CW(filename_uuid);
 
+%CW_over_CW_max = zeros(14,15);
+%P_wave = zeros(14,15);
+%CW_max = zeros(14,15);
+
 [T,Hs] = meshgrid(p.T,p.Hs);
 figure
 
 subplot(2,5,1)
 sub_one=subplot(251)
-sub_one.Position=[0.07, 0.6039, 0.2, 0.3068];
+sub_one.Position=[0.05, 0.5838, 0.24, 0.3412];
 contourf(T,Hs,P_wave);
 xlabel('Wave Period T (s)')
 ylabel('Wave Height Hs (m)')
-title('Raw Wave Power Density (W/m)','FontSize',12)
+title('Raw Wave Power Density (W/m)', 'FontSize', 12, 'Position', [11, 7, 0])
 colorbar
 
 subplot(2,5,2)
 sub_two=subplot(252);
-sub_two.Position=[0.2928, 0.5838, 0.05, 0.3412];
-text(0.5,0.5,'x','FontSize',24)
+sub_two.Position=[0.315, 0.5838, 0.01, 0.3412];
+text(0.5,0.5,'x','FontSize',30)
 axis off
 
 subplot(2,5,3)
 sub_three=subplot(253);
-sub_three.Position=[0.4156, 0.5838, 0.2, 0.3412];
+sub_three.Position=[0.3900, 0.5838, 0.24, 0.3412];
 contourf(T,Hs,CW_over_CW_max)
 xlabel('Wave Period T (s)')
 ylabel('Wave Height Hs (m)')
-title('Device Capture Efficiency','FontSize',12)
+title('Device Capture Efficiency','FontSize',12, 'Position', [11.75, 7, 0])
 colorbar
 
 subplot(2,5,4)
 sub_four=subplot(254);
-sub_four.Position=[0.6484, 0.5838, 0.05, 0.3412];
-text(0.5,0.5,'x','FontSize',24)
+sub_four.Position=[0.650, 0.5838, 0.01, 0.3412];
+text(0.5,0.5,'x','FontSize',30)
 axis off
 
 subplot(2,5,5)
 sub_five=subplot(255);
-sub_five.Position=[0.7713, 0.5838, 0.2, 0.3412];
+sub_five.Position=[0.7250, 0.5838, 0.23, 0.3412];
 contourf(T,Hs,CW_max)
 xlabel('Wave Period T (s)')
 ylabel('Wave Height Hs (m)')
-title('Radiation Capture Width Limit (m)','FontSize',12)
+title('Radiation Capture Width Limit (m)','FontSize',12, 'Position', [11.75, 7, 0])
 colorbar
 
 subplot(2,5,6)
 sub_six=subplot(256);
-sub_six.Position=[0.12, 0.1100, 0.05, 0.3412];
-text(0.5,0.5,'x','FontSize',24)
+sub_six.Position=[0.19, 0.1100, 0.01, 0.3412];
+text(0.55,0.5,'x','FontSize',30)
 axis off
 
 subplot(2,5,7)
 sub_seven=subplot(257);
-sub_seven.Position=[0.2628, 0.1100, 0.2, 0.3412];
+sub_seven.Position=[0.2628, 0.1100, 0.26, 0.3412];
 contourf(T,Hs,p.JPD)
 xlabel('Wave Period T (s)')
 ylabel('Wave Height Hs (m)')
@@ -64,13 +68,13 @@ colorbar
 
 subplot(2,5,8)
 sub_eight=subplot(258);
-sub_eight.Position=[0.5056, 0.1100, 0.05, 0.3412];
-text(0.5,0.5,'=','FontSize',24)
+sub_eight.Position=[0.5496, 0.1100, 0.01, 0.3412];
+text(0.52,0.5,'=','FontSize',30)
 axis off
 
 subplot(2,5,9)
 sub_nine=subplot(259);
-sub_nine.Position=[0.6684, 0.1100, 0.2, 0.3412];
+sub_nine.Position=[0.6350, 0.1100, 0.26, 0.3412];
 contourf(T,Hs,P_wave .* CW_over_CW_max .* CW_max .* p.JPD)
 xlabel('Wave Period T (s)')
 ylabel('Wave Height Hs (m)')
