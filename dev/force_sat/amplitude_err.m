774fa7b3e1337f14113bfcbbe40cb690e5b5c654
Madison Dietrich <mjd429@cornell.edu>
2025-03-26 20:30:13 -0400

    merge

---
+++ dev/force_sat/amplitude_err.m
@@ -0,0 +1,11 @@
+% notebook p113 2/7/25
+% this is the amplitude ratio assuming zeta and wn vs freq are const
+% I would need to combine this with the wave spectrum to get % energy accounted for
+% and technically also combine with zeta and wn vs freq and excitation force vs freq
+
+syms a positive
+syms b real
+syms n integer positive
+trm = 1/ sqrt( a*(1+2*n)^4 + b*(1+2*n)^2 + 1);
+s = symsum(trm,n,1,Inf);
+simplify(s,'Steps',100)
