34cff90b396dfb27d8987d992d4c016b3cbd2885
rebeccamccabe <chessgirl2400@gmail.com>
2025-04-01 22:03:58 -0400

    comment out tests to just run wecsim

--- tests/run_tests.m
+++ tests/run_tests.m
@@ -34,49 +34,49 @@ if run_wecsim_validation && exist('../WEC-Sim','dir')
     cd("..")
 end
 
-suite = testsuite('tests');
-if ~run_wecsim_validation
-    suite = selectIf(suite,~HasName(ContainsSubstring("dynamics"))); % filter out wecsim tests
-end
-runner = testrunner('textoutput');
-
-date = datestr(now,'yyyy-mm-dd_HH.MM.SS');
-cov_dir = ['code-coverage/' date];
-test_dir = ['test-results/' date];
-mkdir(cov_dir)
-mkdir(test_dir)
-
-reportFormat = [CoverageReport([cov_dir '/coverageReport']) CoberturaFormat([cov_dir '/coverage.xml'])];
-dirOut = dir(fullfile(sourceCodeFolder, '**', '*.m'));
-codeFilePaths = string({dirOut.folder}) + filesep + string({dirOut.name});
-% don't test coverage of generated files
-filePathsToExclude = contains(codeFilePaths, 'generated');
-codeFilePaths(filePathsToExclude) = [];
-
-p1 = CodeCoveragePlugin.forFile(codeFilePaths, 'Producing', reportFormat);
-p2 = XMLPlugin.producingJUnitFormat([test_dir '/junit.xml']);
-p3 = TestReportPlugin.producingPDF([test_dir '/testreport.pdf'],'IncludingPassingDiagnostics',true);
-p4 = DiagnosticsRecordingPlugin('IncludingPassingDiagnostics',true);
-
-runner.addPlugin(p1);
-runner.addPlugin(p2);
-runner.addPlugin(p3);
-runner.addPlugin(p4);
-
-runner.ArtifactsRootFolder = test_dir;
-
-tic
-results = runner.runInParallel(suite);
-clockTime = toc;
-
-if ~batchStartupOptionUsed % don't open reports when running on CI server 
-    open([cov_dir '/coverageReport/index.html'])
-    open([test_dir '/testreport.pdf'])
-end
-
-fig = optim_time_bar_chart(suite,results);
-save_pdf(fig,'test-results/Figure_30.pdf')
-
-display(results);
-fprintf('Testing took %g minutes',clockTime/60)
-assertSuccess(results);
+% suite = testsuite('tests');
+% if ~run_wecsim_validation
+%     suite = selectIf(suite,~HasName(ContainsSubstring("dynamics"))); % filter out wecsim tests
+% end
+% runner = testrunner('textoutput');
+% 
+% date = datestr(now,'yyyy-mm-dd_HH.MM.SS');
+% cov_dir = ['code-coverage/' date];
+% test_dir = ['test-results/' date];
+% mkdir(cov_dir)
+% mkdir(test_dir)
+% 
+% reportFormat = [CoverageReport([cov_dir '/coverageReport']) CoberturaFormat([cov_dir '/coverage.xml'])];
+% dirOut = dir(fullfile(sourceCodeFolder, '**', '*.m'));
+% codeFilePaths = string({dirOut.folder}) + filesep + string({dirOut.name});
+% % don't test coverage of generated files
+% filePathsToExclude = contains(codeFilePaths, 'generated');
+% codeFilePaths(filePathsToExclude) = [];
+% 
+% p1 = CodeCoveragePlugin.forFile(codeFilePaths, 'Producing', reportFormat);
+% p2 = XMLPlugin.producingJUnitFormat([test_dir '/junit.xml']);
+% p3 = TestReportPlugin.producingPDF([test_dir '/testreport.pdf'],'IncludingPassingDiagnostics',true);
+% p4 = DiagnosticsRecordingPlugin('IncludingPassingDiagnostics',true);
+% 
+% runner.addPlugin(p1);
+% runner.addPlugin(p2);
+% runner.addPlugin(p3);
+% runner.addPlugin(p4);
+% 
+% runner.ArtifactsRootFolder = test_dir;
+% 
+% tic
+% results = runner.runInParallel(suite);
+% clockTime = toc;
+% 
+% if ~batchStartupOptionUsed % don't open reports when running on CI server 
+%     open([cov_dir '/coverageReport/index.html'])
+%     open([test_dir '/testreport.pdf'])
+% end
+% 
+% fig = optim_time_bar_chart(suite,results);
+% save_pdf(fig,'test-results/Figure_30.pdf')
+% 
+% display(results);
+% fprintf('Testing took %g minutes',clockTime/60)
+% assertSuccess(results);
