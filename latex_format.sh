22cc0ddef791ace42d5ce67f39b6592a0e0491d1
rebeccamccabe <chessgirl2400@gmail.com>
2025-06-06 02:23:53 -0400

    format latex sentences breaks but not after Tab.

--- latex_format.sh
+++ latex_format.sh
@@ -2,7 +2,7 @@
   next if /^\s*%/;  # Skip full-line comments
   my ($code, $comment) = split(/(?<!\\)%/, $_, 2);  # Split code and comment (handle escaped %)
   $code =~ s{
-    (?<!\b(?:e\.g|i\.e|etc|vs|Mr|Ms|Dr|Prof|Fig|Eq|cf|et\ al|U\.S))  # avoid common abbreviations
+    (?<!\b(?:e\.g|i\.e|etc|vs|Mr|Ms|Dr|Prof|Fig|Eq|Tab|cf|et\ al|U\.S))  # avoid common abbreviations
     ([.!?])                           # sentence-ending punctuation
     (\s+)(?=[A-Z\\])                  # followed by space + capital or command
   }{$1\n}gx;
