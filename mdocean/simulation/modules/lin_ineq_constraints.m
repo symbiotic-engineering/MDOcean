365c51e30a7c2761be0934cd5201bbe79007fe43
rebeccamccabe <chessgirl2400@gmail.com>
2025-06-12 19:47:52 -0400

    fix lin constraints: h_stiff,f scaling and change -.01 to 0 for non priority 1 constraints

--- mdocean/simulation/modules/lin_ineq_constraints.m
+++ mdocean/simulation/modules/lin_ineq_constraints.m
@@ -12,10 +12,10 @@ A = [-p.D_d_over_D_s                  0  0                        0  0  0  0  0 
       0                             MEEM 1                        0  0  0  0  0  0  0   0   0   0; % g_lin_5 
       MEEM + p.T_s_over_D_s,          0  0                        0  0  0  0  0  0  0   0   0   0; % g_lin_6 
      -p.h_d_over_D_s                  0  0                        0  0  0  0  0  0 1e-3 0   0   0; % g_lin_7 
-      0                               0  -.5/p.T_f_2_over_h_f,    0  0  0  0  0  0  0   1   0   0];% g_lin_8
+      0                               0  -.5/p.T_f_2_over_h_f,    0  0  0  0  0  0  0   .1  0   0];% g_lin_8
 
-%     g1         g2    g3    g4   g5  g6   g7    g8
-b = [-p.D_d_min -0.01 -0.01 -0.01 p.h p.h -0.01 -0.01]';
+%     g1         g2    g3  g4  g5   g6   g7  g8
+b = [-p.D_d_min -0.01  0   0   p.h  p.h  0   0]';
 
 
 if nargout > 2 && nargin > 1
