cfaf545404c9a0f8950849b9dfb75eec4fdca2aa
anthnlong <anthonylong0134@gmail.com>
2025-07-01 23:10:48 -0400

    phase included

--- dev/dynamics/force_sat/bode_noRAO.m
+++ dev/dynamics/force_sat/bode_noRAO.m
@@ -1,22 +1,5 @@
 clear
-
-%{
-lwb = 6;
-
-omega = [1.142, 0.967, 0.838, 0.739, 0.661, 0.598, 0.546, ...
-         0.503, 0.465, 0.433, 0.405, 0.381]; % Frequency (rad/s)
-omega = omega(lwb:end);
-
-mag_lin = [7.19e-07, 4.90e-07, 4.16e-07, 3.82e-07, 3.79e-07, ...
-           4.51e-07, 8.72e-07, 1.00e-06, 6.68e-07, 5.36e-07, ...
-           4.75e-07, 4.42e-07]; % Magnitude (linear)
-mag_lin = mag_lin(lwb:end);
-
-phase = [-0.009174, -0.041284, -0.077982, -0.114679, ...
-         -0.169725, -0.220596, -0.270222, -0.310012, ...
-         -0.348858, -0.387704, -0.428700, -0.471168]; % Phase (radians)
-phase = phase(lwb:end);
-%}
+close all
 
 p = parameters();
 b = var_bounds();
@@ -53,10 +36,17 @@ mag_db = 20 * log10(mag_matrix);
 % pull data from val/rename
 omega = w;
 
-% setup fit model
+% setup fit model for magnitude
 mag_model = fittype(@(omega_n, k, zeta, w) ...
     20*log10(abs(((omega_n^2) .* k) ./ (-w.^2 + 1i*2*zeta*omega_n.*w + omega_n^2))), ...
     'independent', 'w', 'coefficients', {'omega_n', 'k', 'zeta'});
+
+% setup fit model for angle
+angle_model = fittype(@(omega_n, k, zeta, w) ...
+    20*log10(abs(((omega_n^2) .* k) ./ (-w.^2 + 1i*2*zeta*omega_n.*w + omega_n^2))), ...
+    'independent', 'w', 'coefficients', {'omega_n', 'k', 'zeta'});
+
+
 
 %{
 mag_data   = mag_matrix(5,:);
@@ -77,8 +67,13 @@ TF_frd = frd(TF_data(~isnan(TF_data)), TF_w(~isnan(TF_w)));
 avgs = mean(omega, 1, 'omitnan')
 
 
-% cutoff for limiting points
+% cutoff for limiting points/minimum fit points required. This is due to a
+% minimum of 3 points required for fittype to work, and since the highest
+% <lwb> frequencies are discarded due to incorrect model assumptions, some
+% of the fits have very little points to work off of which results in
+% inaccurate models, the amount of points which can be controlled by <tol>
 lwb = 5;
+tol = 4;
 
 for i = 1:size(omega,1)
     mags = mag_db(i, :);
@@ -92,62 +87,71 @@ for i = 1:size(omega,1)
     col = [color_frac 0 1-color_frac];
     %mag_norm = mag_data * norm;
     
-    %nexttile(1)
-    loglog(omegas/wn, mags, '*-', ...
-        'DisplayName',['H_s=' num2str(p.Hs(i))],'Color',col)
+    nexttile(1)
+    
+    ylabel(['Magnitude |' y_lab '| ' y_lab_extra ' (-)'])
+    %xlim([.5 omega_over_omega_n_max])
+
+    wn = 0.54 % placeholder will be replaced later
+    loglog(omegas, mags, '*-', ...
+        'DisplayName',['H_s=' num2str(p.Hs(i))],'Color',col) % TODO: normalize by wn later
     hold on
 
     
-    %nexttile(2)
-    %h = semilogx(omegas, angles/pi,'*-','Color',col);
-    %h.Annotation.LegendInformation.IconDisplayStyle = 'off'; 
-    %hold on
+    nexttile(2)
+
+    xlabel('\omega/\omega_n (-)')
+    ylabel(['Phase \angle(' y_lab ') / \pi'])
+
+    h = semilogx(omegas, angles/pi,'*-','Color',col);
+    h.Annotation.LegendInformation.IconDisplayStyle = 'off'; 
+    hold on
     
 
 
-    % remove NaN values + extreme values editable with lwb
+    % remove NaN values + high freq values editable with lwb
+
     mags_clean = mags(1, lwb:end);
     omegas_clean = omegas(1, lwb:end);
+    angles_clean = omegas(1, lwb:end);
     
     omegas_clean(isnan(omegas_clean)) = avgs(isnan(omegas_clean));
-    omegas_clean = omegas_clean(~isnan(mags_clean));
+    omegas_clean_mag = omegas_clean(~isnan(mags_clean));
+    omegas_clean_angle = omegas_clean(~isnan(angles_clean));
 
     mags_clean = mags_clean(~isnan(mags_clean));
+    angles_clean = angles_clean(~isnan(angles_clean));
 
     
 
     % input fits and create fit models
 
-    if size(mags_clean, 2) > 5
+    if size(mags_clean, 2) > tol
 
-    [mag_fit, gof_mag] = fit(omegas_clean.', mags_clean.', mag_model, ...
+    [mag_fit, gof_mag] = fit(omegas_clean_mag.', mags_clean.', mag_model, ...
     'StartPoint', [0.5, 1e-6, 0.1]);
 
     % get results from fit
-
-
+    
     w_fit = logspace(log10(min(omega, [], "all", "omitnan")), log10(max(omega,[], "all", "omitnan")), 500);
 
     mag_fit_vals = 20*log10(abs((mag_fit.k .* mag_fit.omega_n^2) ./ ...
     (-w_fit.^2 + 1i*2*mag_fit.zeta*mag_fit.omega_n.*w_fit + mag_fit.omega_n^2)));
 
-    %fit_mag = 20*log10(abs(((wn^2) .* mag_fit.k) ./ (-w.^2 + 1i*2*mag_fitzeta*wn.*w + wn^2)))
-
-    omega_over_omega_n_max = max(omega/wn,[],'all');
+    omega_over_omega_n_max = max(omega,[],'all'); % TODO: implement normalization by wn later
 
     %fplot(omega,mag_fit_vals,[0 omega_over_omega_n_max],...
     %    'DisplayName',['\zeta=' num2str(zeta(i))],'Color',(i-1)/length(zeta)*[1 1 1])
-
+    nexttile(1)
         plot(w_fit.',mag_fit_vals.',...
         'DisplayName',['\zeta=' num2str(i)],'Color',(i-1)/i*[1 1 1])
     
     else
 
-    disp('failed')
+    disp('failed - too few points')
     
     end
     
-
 end
 improvePlot
 %{
