c3a314630ba64b7f197f5f23e79266fbce4b09d1
anthnlong <anthonylong0134@gmail.com>
2025-08-05 12:05:58 -0400

    dummy data for CEM

--- mdocean/simulation/modules/grid_CEM.m
+++ mdocean/simulation/modules/grid_CEM.m
@@ -29,18 +29,70 @@ function [zeta, omega_n] = fit_second_order_sys(X_u, phase_X_u, gamma_f_over_rho
 
 end
 
+function row = findNearestRow(zeta0, omega_n0, capCost0, T)
+
+
+    % Extract numeric arrays
+    z = T.zeta;
+    w = T.omega_n;
+    c = T.capacity_cost;
+
+    % Check ranges
+    if zeta0 < min(z) || zeta0 > max(z)
+       disp(zeta0)
+        error('zeta inputs are out of range.');
+        
+    end
+
+    if   omega_n0 < min(w) || omega_n0 > max(w)
+        error('omega are out of range.');
+    end
+
+    if capCost0 < min(c) || capCost0 > max(c)
+        error('capCost out of range')
+
+    end
+
+    % find distance (placeholder interp code, fixme)
+    difference = (z - zeta0).^2 + (w - omega_n0).^2 + (c - capCost0).^2;
+
+    % Find index of minimum distance
+    [~, idx] = min(difference);
+
+    % Return that row
+    row = T(idx, :);
+end
+
+
+
 function [CEM_CO2, CEM_wec_capacity, CEM_grid_cost] = CEM_lookup_table(zeta, omega_n, capacity_cost, B_p, location)
-    %CEM_co2 = interpn(zeta_data, omega_n_data, cap_cost_data, power_lim_data, CO2_data,...
+
+    dummy = readtable('dummy3.csv') % this is just placeholder for now, replace with actual data
+    %{
+    CEM_co2 = interpn(zeta_data, omega_n_data, cap_cost_data, power_lim_data, CO2_data,...
+        zeta,omega_n,capacity_cost,power_limit);
+    CEM_capacity = interpn(zeta_data, omega_n_data, cap_cost_data, power_lim_data, capacity_data,...
+        zeta,omega_n,capacity_cost,power_limit);
+    %}
+
+    %CEM_co2 = interpn(dummy.zeta, dummy.omega_n, cap_cost_data, power_lim_data, CO2_data,...
     %    zeta,omega_n,capacity_cost,power_limit);
     %CEM_capacity = interpn(zeta_data, omega_n_data, cap_cost_data, power_lim_data, capacity_data,...
     %    zeta,omega_n,capacity_cost,power_limit);
 
     
     %if zeta == 0.05 && omega_n == 0.4 && strcmp(location,'ISONE')
+    
+    CEM_data = findNearestRow(zeta, omega_n, capacity_cost, dummy)
 
-    isValidLookupLocation= true; % fixme: placeholder for now until CEM data comes in
+    disp(CEM_data)
+
+    isValidLookupLocation = true;% placeholder
+    
     if (isValidLookupLocation)
         % fixme put real lookup table here
+
+
         co2_slope = 2/3;
         cost_slope = 1/3;
         cap_slope = 2.5/3;
