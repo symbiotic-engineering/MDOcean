7e805fdadf35c02ab3b978d1fae67bc4659d304b
rebeccamccabe <chessgirl2400@gmail.com>
2025-08-31 20:46:02 -0400

    fix check_figures index error for when all are ok

--- mdocean/plots/check_figures.m
+++ mdocean/plots/check_figures.m
@@ -1,4 +1,4 @@
-[fig_success,tab_success, fig_output, tab_output,fig_runtime,tab_runtime] = all_figures();
+[fig_success,tab_success, fig_output, tab_output,fig_runtime,tab_runtime] = all_figures(19,[]);
 
 fig_success_copy = fig_success;
 idx_success = cellfun(@isempty,fig_success);
@@ -10,8 +10,10 @@ stack = cellfun(@(c) c.stack, fig_success_copy, 'UniformOutput', false);
 file = cell(size(fig_success_copy));
 top_stack_cell = cellfun(@(x) x(1), stack(~idx_success), 'UniformOutput', false);
 top_stack = [top_stack_cell{:}];
-file(~idx_success) = {top_stack.name};
-
 line = zeros(size(fig_success_copy));
-line(~idx_success) = [top_stack.line];
+if ~isempty(top_stack)
+    file(~idx_success) = {top_stack.name};
+    line(~idx_success) = [top_stack.line];
+end
+
 table((1:length(msg)).', msg, file, line, 'VariableNames', {'#','Error Message','File','Line'})
