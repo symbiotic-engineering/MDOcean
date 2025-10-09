4f9227c2c27564cccbb5eae9866892425e6c5c36
Rebecca McCabe <chessgirl2400@gmail.com>
2025-10-09 00:54:35 -0400

    run only fig 27 (param sens) and 30 (pareto design images)

--- tests/test.m
+++ tests/test.m
@@ -4,10 +4,10 @@ classdef (SharedTestFixtures={ ...
     % class based unit tests, as in https://www.mathworks.com/help/matlab/matlab_prog/class-based-unit-tests.html
     
     properties (Constant)
-        run_slow_tests = true;
+        run_slow_tests = false;
 
-        slow_figs = [25:35, 53:55, 58];
-        slow_tabs = 8;
+        slow_figs = [1:26,28,29,31:59]; %[25:35, 53:55, 58];
+        slow_tabs = 1:8; % 8;
     end
 
     properties
