3cfc96ceb930c62ead6bf38df8a964f7f1b10742
rebeccamccabe <chessgirl2400@gmail.com>
2025-10-10 02:52:45 -0400

    Revert "temporarily only run first 3 figs for speed"
    This reverts commit 52431fbaa7fa619a222bb4d4fb31c0b0ef957680.

--- tests/test.m
+++ tests/test.m
@@ -4,9 +4,9 @@ classdef (SharedTestFixtures={ ...
     % class based unit tests, as in https://www.mathworks.com/help/matlab/matlab_prog/class-based-unit-tests.html
     
     properties (Constant)
-        run_slow_tests = false;
+        run_slow_tests = true;
 
-        slow_figs = [4:59];
+        slow_figs = [16:25];
         slow_tabs = 1:8;
     end
 
