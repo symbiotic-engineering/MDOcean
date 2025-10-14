9d111f45f2f6e86559a9b4e3a258d40ee282f4c2
rebeccamccabe <chessgirl2400@gmail.com>
2025-10-14 00:21:33 -0400

    add pwd on no analysis error for debugging

--- mdocean/analysis/get_all_analyses.m
+++ mdocean/analysis/get_all_analyses.m
@@ -5,7 +5,9 @@ function [analysis_list] = get_all_analyses()
     if isempty(analysis_list)
         analysis_list = dir('mdocean/analysis/*.m');
         if isempty(analysis_list)
-            error('mdocean:get_all_analyses:no analyses found')
+            msg = ['No analyses found. Current dir: ' pwd];
+            ME = MException('mdocean:get_all_analyses:no_analyses', msg);
+            throw(ME);
         end
     end
     analysis_list = {analysis_list.name};
