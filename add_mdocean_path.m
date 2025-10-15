b2e6a460cb665927f475f1ebd0ad2b7cc0afdb5b
Rebecca McCabe <chessgirl2400@gmail.com>
2025-10-14 23:23:01 -0400

    include wecsim in add_mdocean_path

--- add_mdocean_path.m
+++ add_mdocean_path.m
@@ -9,4 +9,18 @@ s = split(which(path), filesep);
 MDOcean_folder = strjoin(s(1:end-1), filesep);
 mdocean_folder = [MDOcean_folder filesep 'mdocean'];
 addpath(genpath(mdocean_folder))
-clear path s MDOcean_folder mdocean_folder
+
+rmpath(genpath([mdocean_folder '/simulation/modules/OpenFLASH'])) % prevent using OpenFLASH run_MEEM since it's not integrated yet
+
+load_sl_glibc_patch % for linux, see https://www.mathworks.com/support/bugreports/2632298
+
+wecSimFolder = [MDOcean_folder filesep '../WEC-Sim'];
+if exist(wecSimFolder,'dir')    
+    wecSimSourceFolder = [wecSimFolder filesep 'source'];
+    set_param(0, 'ErrorIfLoadNewModel', 'off')
+    addpath(genpath(wecSimSourceFolder))
+    rmpath([wecSimSourceFolder '/functions/BEMIO/readWAMIT.m'])
+    clear wecSimSourceFolder
+end
+
+clear path s MDOcean_folder mdocean_folder wecSimFolder
