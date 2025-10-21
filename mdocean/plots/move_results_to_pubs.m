d1340fd3a7862d41835a4440cea52b629668db96
Rebecca McCabe <chessgirl2400@gmail.com>
2025-10-21 18:30:47 -0400

    change fig/tab folder structure, adding from matlab folder in pubs and fixing path when moving

--- mdocean/plots/move_results_to_pubs.m
+++ mdocean/plots/move_results_to_pubs.m
@@ -2,22 +2,26 @@ function move_results_to_pubs()
 
     [figs_in_RE, figs_in_AOR, tabs_in_RE, tabs_in_AOR] = fig_tab_pub_mapping();
 
-    RE = 'pubs/renewable-energy-mdo';
-    AOR = 'pubs/applied-ocean-research-model';
+    RE =  '../../pubs/renewable-energy-mdo';
+    AOR = '../../pubs/applied-ocean-research-model';
 
     success = true;
 
     for i = 1:length(figs_in_RE)
-        success = success && copy_result(figs_in_RE{i}, '.pdf', RE);
+        success_i = copy_result(figs_in_RE{i}, '.pdf', [RE '/figs/from-matlab']);
+        success = success && success_i;
     end
     for i = 1:length(figs_in_AOR)
-        success = success && copy_result(figs_in_AOR{i}, '.pdf', AOR);
+        success_i = copy_result(figs_in_AOR{i}, '.pdf', [AOR '/figs/from-matlab']);
+        success = success && success_i;
     end
     for i = 1:length(tabs_in_RE)
-        success = success && copy_result(tabs_in_RE{i}, '.tex', RE);
+        success_i = copy_result(tabs_in_RE{i}, '.tex', [RE '/tables/from-matlab']);
+        success = success && success_i;
     end
     for i = 1:length(tabs_in_AOR)
-        success = success && copy_result(tabs_in_AOR{i}, '.tex', AOR);
+        success_i = copy_result(tabs_in_AOR{i}, '.tex', [AOR '/tables/from-matlab']);
+        success = success && success_i;
     end
     assert(success, 'Some files were not found and could not be copied.')
 end
@@ -26,9 +30,12 @@ function success = copy_result(class_dot_description, extension, dest_folder)
     s = split(class_dot_description, '.');
     class_name = s{1};
     fig_tab_desc = s{2};
-    src = fullfile('results', class_name, [fig_tab_desc, extension]);
+    src = fullfile('..', '..', 'results', class_name, [fig_tab_desc, extension]);
     dest = fullfile(dest_folder, [fig_tab_desc, extension]);
-    if exist(src,"file")
+    if isfile(src)
+        if ~isfolder(dest_folder)
+            mkdir(dest_folder);
+        end
         copyfile(src, dest);
         success = true;
     else
