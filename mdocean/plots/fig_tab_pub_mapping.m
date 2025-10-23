ec75776e929ab47ef4023bb7f511090186508ce7
Rebecca McCabe <chessgirl2400@gmail.com>
2025-10-23 14:50:19 -0400

    update RE figure pub mapping

--- mdocean/plots/fig_tab_pub_mapping.m
+++ mdocean/plots/fig_tab_pub_mapping.m
@@ -13,36 +13,36 @@ figs_in_RE{1}  = 'ReadNonMatlabFigs.RM3_image';
 % 2: modeling methodology
 figs_in_RE{2}  = 'ReadNonMatlabFigs.methodology_overview';
 figs_in_RE{3}  = 'ReadNonMatlabFigs.N2_diagram';
-figs_in_RE{4}  = 'ReadNonMatlabFigs.dimensions';
-% 3: optimization methodology
-figs_in_RE{5} = 'ReadNonMatlabFigs.optimization_flowchart';
+% archs
+figs_in_RE{4} = 'ReadNonMatlabFigs.optimization_flowchart';
 % 4: results
-figs_in_RE{6} = 'DesignSpaceExploration.experiments';
-figs_in_RE{7} = 'Comparison.overlaid_geometry';
-figs_in_RE{8} = 'Comparison.overlaid_hydro_coeffs';
-figs_in_RE{9} = 'Comparison.probability_CDF';
-figs_in_RE{10} = 'GradientOptimFigFunc.single_obj_opt_power_matrix';
-figs_in_RE{11} = 'GradientOptimFigFunc.lagrange_multipliers';
-figs_in_RE{12} = 'GradientOptimFigFunc.dJ_dx_gradient';
-figs_in_RE{13} = 'Multistart.multistart_convergence_tree';
-figs_in_RE{14} = 'Multistart.multistart_bar_chart';
-figs_in_RE{15} = 'ParamSensitivities.re_optim_objective_tornado';
-figs_in_RE{16} = 'ParamSensitivities.re_optim_design_tornado_J1';
-figs_in_RE{17} = 'ParamSensitivities.re_optim_design_tornado_J2';
-figs_in_RE{18} = 'ParetoFigFunc.pareto_front_with_design_images';
-figs_in_RE{19} = 'ParetoFigFunc.pareto_front_LCOE_contours';
-figs_in_RE{20} = 'ParetoFigFunc.pareto_heuristics';
-figs_in_RE{21} = 'ParetoFigFunc.pareto_constraint_activity';
+figs_in_RE{5} = 'DesignSpaceExploration.experiments';
+figs_in_RE{6} = 'Comparison.overlaid_geometry';
+figs_in_RE{7} = 'Comparison.overlaid_hydro_coeffs';
+figs_in_RE{8} = 'Comparison.probability_CDF';
+figs_in_RE{9} = 'GradientOptimFigFunc.single_obj_opt_power_matrix';
+figs_in_RE{10} = 'GradientOptimFigFunc.lagrange_multipliers';
+figs_in_RE{11} = 'GradientOptimFigFunc.delta_x';
+figs_in_RE{12} = 'Multistart.multistart_convergence_tree';
+figs_in_RE{13} = 'Multistart.multistart_bar_chart';
+figs_in_RE{14} = 'ParamSensitivities.re_optim_objective_tornado';
+figs_in_RE{15} = 'ParamSensitivities.re_optim_design_tornado_J1';
+figs_in_RE{16} = 'ParamSensitivities.re_optim_design_tornado_J2';
+figs_in_RE{17} = 'ParetoFigFunc.pareto_front_with_design_images';
+figs_in_RE{18} = 'ParetoFigFunc.pareto_front_LCOE_contours';
+figs_in_RE{19} = 'ParetoFigFunc.pareto_heuristics';
+figs_in_RE{20} = 'ParetoFigFunc.pareto_constraint_activity';
+figs_in_RE{21} = 'ParetoFigFunc.pareto_damping_reactive';
 figs_in_RE{22} = 'AllFigCompare.runtime_bar_chart';
 figs_in_RE{23} = 'ParetoSweep.sweep_num_seeds';
+% appendix A - supplementary results
+figs_in_RE{24} = 'GradientOptimFigFunc.normalized_gradient_gradient';
+figs_in_RE{25} = 'GradientOptimFigFunc.single_obj_convergence';
+figs_in_RE{26} = 'Multistart.multistart_parallel_coordinates';
 % appendix E - optimization process
-figs_in_RE{24} = 'ParamSensitivities.post_optim_re_optim_objective_grid';
-figs_in_RE{25} = 'ParamSensitivities.post_optim_design_grid';
-figs_in_RE{26} = 'ParamSensitivities.re_optim_design_grid';
-% appendix F - supplementary results
-figs_in_RE{27} = 'GradientOptimFigFunc.normalized_gradient';
-figs_in_RE{28} = 'GradientOptimFigFunc.single_obj_convergence';
-figs_in_RE{29} = 'Multistart.multistart_parallel_coordinates';
+figs_in_RE{27} = 'ParamSensitivities.post_optim_re_optim_objective_grid';
+figs_in_RE{28} = 'ParamSensitivities.post_optim_design_grid';
+figs_in_RE{29} = 'ParamSensitivities.re_optim_design_grid';
 % graphical abstract (unnumbered so at the end)
 figs_in_RE{30}  = 'ReadNonMatlabFigs.graphical_abstract';
 
