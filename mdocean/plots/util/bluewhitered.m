8e9e746beb89bd95abbd7ddb98758717c85a61c7
rebeccamccabe <chessgirl2400@gmail.com>
2025-12-01 01:57:01 -0500

    bluewhitered: add flip and full_color options, and fix color logic where toponly or bottom only maps included white even if out of range

--- mdocean/plots/util/bluewhitered.m
+++ mdocean/plots/util/bluewhitered.m
@@ -1,10 +1,19 @@
-function newmap = bluewhitered(m, white_number)
+function newmap = bluewhitered(m, white_number, flip, full_color)
 %BLUEWHITERED   Blue, white, and red color map.
 %   BLUEWHITERED(M) returns an M-by-3 matrix containing a blue to white
 %   to red colormap, with white corresponding to the CAXIS value closest
 %   to zero.  This colormap is most useful for images and surface plots
 %   with positive and negative values.  BLUEWHITERED, by itself, is the
 %   same length as the current colormap.
+% white_number (default zero) is the number to correspond to white.
+% (Sometimes this is set to 1 if plotting a ratio).
+% flip (default false) is a boolean for whether to use red-white-blue instead
+% of blue-white-red.
+% full_color (default false) is a boolean for whether the entire range of
+% blue and red shades should be shown even if it results in uneven color
+% gradient. for example, numerical limits of [-1,100] will have colors of
+% [nearly-white-pale-blue, full-red] when full_color is false and
+% [full-blue, full-red] when full_color is true.
 
 % Adapted from https://www.mathworks.com/matlabcentral/fileexchange/4058-bluewhitered
 %
@@ -37,50 +46,78 @@ end
 if nargin < 2
     white_number = 0;
 end
+if nargin < 3
+    flip = false;
+end
+if nargin < 4
+    full_color = false;
+end
 
-bottom_blue = [0 0 0.5];
-botmiddle_blue = [0 0.5 1];
-middle_white = [1 1 1];
-topmiddle_red = [1 0 0];
-top_red = [0.5 0 0];
+% define rgb colors
+blue       = [0   0   0.5];
+light_blue = [0   0.5 1];
+white      = [1   1   1];
+red        = [1   0   0];
+dark_red   = [0.5 0   0];
 
 % Find limits
 lims = get(gca, 'CLim');
 
-bluemap_basic = [bottom_blue; botmiddle_blue; middle_white];
-redmap_basic = [middle_white; topmiddle_red; top_red];
+if ~flip % blue white red
+    botmap_basic = [blue; light_blue; white];
+    topmap_basic = [white; red; dark_red];
+else % red white blue
+    botmap_basic = [dark_red; red; white];
+    topmap_basic = [white; light_blue; blue];
+end
 
-bluepart = white_number - lims(1);
-redpart = lims(2) - white_number;
+botpart = white_number - lims(1);
+toppart = lims(2) - white_number;
 
-if bluepart > 0 && redpart > 0
-    % It has both blue and red
-    ratio_blue = bluepart / (bluepart + redpart);
-    bluelen = round(m*ratio_blue);
-    redlen = m - bluelen;
+if botpart > 0 && toppart > 0
+    % It has both bot (blue) and top (red)
+    ratio_bot = botpart / (botpart + toppart);
+    botlen = round(m*ratio_bot);
+    toplen = m - botlen;
     
-    bluemap = resize_cmap(bluemap_basic,bluelen);
-    redmap = resize_cmap(redmap_basic,redlen);
-    newmap = [bluemap; redmap];
+    if full_color || botlen == toplen
+        frac_bot = 0;
+        frac_top = 1;
+    elseif botlen > toplen
+        frac_bot = 0;
+        frac_top = toplen/botlen;
+    else
+        frac_bot = 1-botlen/toplen;
+        frac_top = 1;
+    end
+
+    botmap = resize_cmap(botmap_basic,botlen,frac_bot,1);
+    topmap = resize_cmap(topmap_basic,toplen,0,frac_top);
+    newmap = [botmap; topmap];
     
-elseif bluepart <= 0
-    % Just red (positive)
-    redmap_basic = [middle_white; topmiddle_red; top_red];
-    newmap = resize_cmap(redmap_basic,m);
+elseif botpart <= 0
+    % Just top (red, positive)
+    frac = -botpart/toppart;
+    newmap = resize_cmap(topmap_basic,m,frac,1);
     
 else
-    % Just blue (negative)
-    bluemap_basic = [bottom_blue; botmiddle_blue; middle_white];
-    newmap = resize_cmap(bluemap_basic,m);
+    % Just bottom (blue, negative)
+    frac = 1+toppart/botpart;
+    newmap = resize_cmap(botmap_basic,m,0,frac);
     
 end
 end
 
-function newmap = resize_cmap(oldmap,newlen)
+function newmap = resize_cmap(oldmap,newlen,newstep_min,newstep_max)
+
+    if nargin==2
+        newstep_min = 0;
+        newstep_max = 1;
+    end
 
     oldlen = length(oldmap);
     oldsteps = linspace(0, 1, oldlen);
-    newsteps = linspace(0, 1, newlen);
+    newsteps = linspace(newstep_min, newstep_max, newlen);
     newmap = zeros(newlen, 3);
     
     for i=1:3
