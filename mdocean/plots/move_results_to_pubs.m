fd5931bbc1feb9efaa0cb843c9f83d8658204c7e
Rebecca McCabe <chessgirl2400@gmail.com>
2025-12-02 00:12:52 -0500

    change paths to make move-results runnable from outer MDOcean folder

--- mdocean/plots/move_results_to_pubs.m
+++ mdocean/plots/move_results_to_pubs.m
@@ -2,8 +2,8 @@ function move_results_to_pubs()
 
     [figs_in_RE, figs_in_AOR, tabs_in_RE, tabs_in_AOR] = fig_tab_pub_mapping();
 
-    RE =  '../../pubs/renewable-energy-mdo';
-    AOR = '../../pubs/applied-ocean-research-model';
+    RE =  'pubs/renewable-energy-mdo';
+    AOR = 'pubs/applied-ocean-research-model';
 
     success = true;
 
@@ -31,7 +31,7 @@ function success = copy_result(class_dot_description, extension, dest_folder)
     s = split(class_dot_description, '.');
     class_name = s{1};
     fig_tab_desc = s{2};
-    src = fullfile('..', '..', 'results', class_name, [fig_tab_desc, extension]);
+    src = fullfile('results', class_name, [fig_tab_desc, extension]);
     dest = fullfile(dest_folder, [fig_tab_desc, extension]);
     if isfile(src)
         if ~isfolder(dest_folder)
