13907ef495a4006d57c17375246f223d8da1b786
rebeccamccabe <chessgirl2400@gmail.com>
2025-12-03 06:38:53 -0500

    add pdf to svg script and insert link to svg in ppt

---
+++ mdocean/plots/util/pdf_to_svg.py
@@ -0,0 +1,20 @@
+import pymupdf
+import sys
+
+# example to call this script: python mdocean/plots/util/pdf_to_svg.py pubs/ezra_seminar_slides/figs/N2_diagram
+
+def pdf_to_svg(filename):
+    doc = pymupdf.open(filename+".pdf")
+    page = doc[0]
+    
+    # Convert page to SVG
+    svg_content = page.get_svg_image()
+    
+    # Save to file
+    with open(filename+".svg", "w", encoding="utf-8") as f:
+        f.write(svg_content)
+    
+    doc.close()
+
+args = sys.argv[1]
+pdf_to_svg(args)
