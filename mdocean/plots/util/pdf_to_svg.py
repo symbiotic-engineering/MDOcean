c0ebb6ad4f1f6fb0af1d37650ce26ae09fa169a6
rebeccamccabe <chessgirl2400@gmail.com>
2025-12-03 15:20:59 -0500

    adjust pdf_to_svg to convert all pdfs in folder; run pipeline

--- mdocean/plots/util/pdf_to_svg.py
+++ mdocean/plots/util/pdf_to_svg.py
@@ -1,20 +1,27 @@
 import pymupdf
 import sys
+import os
+import glob
 
 # example to call this script: python mdocean/plots/util/pdf_to_svg.py pubs/ezra_seminar_slides/figs/N2_diagram
 
-def pdf_to_svg(filename_in,filename_out):
-    doc = pymupdf.open(filename+".pdf")
-    page = doc[0]
-    
-    # Convert page to SVG
-    svg_content = page.get_svg_image()
-    
-    # Save to file
-    with open(filename+".svg", "w", encoding="utf-8") as f:
-        f.write(svg_content)
-    
-    doc.close()
+def pdf_to_svg(folder_in,folder_out):
+    files_in = glob.glob(folder_in + '*.pdf')
+    for file in files_in:
+        doc = pymupdf.open(file)
+        page = doc[0]
+        
+        # Convert page to SVG
+        svg_content = page.get_svg_image()
+        
+        # Save to file
+        base = os.path.basename(file)
+        os.makedirs(folder_out, exist_ok=True)
+        file_out = folder_out + os.path.splitext(base)[0]
+        with open(file_out + ".svg", "w", encoding="utf-8") as f:
+            f.write(svg_content)
+        
+        doc.close()
 
 args = sys.argv[1:]
 pdf_to_svg(args[0],args[1])
