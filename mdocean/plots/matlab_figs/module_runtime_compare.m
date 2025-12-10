fdcc700a4514b7000f90f2d681a26cd2a6cbbb64
rebeccamccabe <chessgirl2400@gmail.com>
2025-12-10 15:15:20 -0500

    replace profile with timeit for benchmarking

--- mdocean/plots/matlab_figs/module_runtime_compare.m
+++ mdocean/plots/matlab_figs/module_runtime_compare.m
@@ -1,27 +1,39 @@
-function [f1, f2, f3] = module_runtime_compare(p,b)
+function [f1, f2, f3, f4] = module_runtime_compare(p,b)
     
     X = [b.X_noms; 1];
+    num_outputs = 3;
 
-    % just dynamics
+    % run sim with timing
     p.use_multibody = true;
     profile clear
     profile on
     simulation(X,p);
-    p_multibody = profile('info');
+    profile_multibody = profile('info');
+    t_multibody_fullsim_timeit = timeit(@()simulation(X,p),num_outputs);
 
     p.use_multibody = false;
     profile clear
     profile on
     simulation(X,p);
-    p_singlebody = profile('info');
+    profile_singlebody = profile('info');
+    t_singlebody_fullsim_timeit = timeit(@()simulation(X,p),num_outputs);
 
-    t_multibody  = extract_runtime(p_multibody, 'get_response_drag');
-    t_singlebody = extract_runtime(p_singlebody,'get_response_drag');
+    % extract full sim timing and multiplier
+    t_multibody_fullsim_profile  = extract_runtime(profile_multibody, 'simulation');
+    t_singlebody_fullsim_profile = extract_runtime(profile_singlebody,'simulation');
+   
+    time_multiplier_multibody  = t_multibody_fullsim_timeit / t_multibody_fullsim_profile;
+    time_multiplier_singlebody = t_singlebody_fullsim_timeit / t_singlebody_fullsim_profile;
+
+    % just dynamics
+    t_multibody  = time_multiplier_multibody  * extract_runtime(profile_multibody, 'get_response_drag');
+    t_singlebody = time_multiplier_singlebody * extract_runtime(profile_singlebody,'get_response_drag');
 
     gcp;
     time = tic;
-    runRM3Parallel
-    t_wecsim = toc(time); %447
+    pause(1)
+%     runRM3Parallel
+    t_wecsim = 447; %toc(time); %447
 
     f1 = figure;
     cats = {'MDOcean Dynamics 1-DOF','MDOcean Dynamics 2-DOF','WecSim (Parallelized)'};
@@ -39,6 +51,8 @@ function [f1, f2, f3] = module_runtime_compare(p,b)
     improvePlot
     h.Parent.YMinorGrid = 'off';
     f1.Position = [100 100 600 640];
+    exponents = log10(times);
+    ylim(10.^[min(floor(exponents)),max(ceil(exponents))]) % one order of magnitude above max and below min
 
     % hydro
     % determine line numbers
@@ -48,12 +62,16 @@ function [f1, f2, f3] = module_runtime_compare(p,b)
     dispersion_lines = findStringInFile('run_MEEM','m_k_h_deg = fzero(eqn, bounds)');
     lin_solve_lines  = findStringInFile('run_MEEM','A_num\b_num');
 
+    % ensure no double counting lines from same file
+    assert(~any(ismember(bessel_lines,unpack_lines)))
+    assert(~any(ismember(dispersion_lines,lin_solve_lines)))
+
     % extract times for line numbers
-    t_hydro            = extract_runtime(p_singlebody,'get_dynamic_coeffs');
-    t_hydro_bessels    = extract_runtime(p_singlebody,'ft_1',bessel_lines);
-    t_hydro_unpack     = extract_runtime(p_singlebody,'ft_1',unpack_lines);
-    t_hydro_dispersion = extract_runtime(p_singlebody,'compute_eigen_hydro_coeffs',dispersion_lines);
-    t_hydro_lin_solve  = extract_runtime(p_singlebody,'compute_eigen_hydro_coeffs',lin_solve_lines);
+    t_hydro            = time_multiplier_singlebody * extract_runtime(profile_singlebody,'get_dynamic_coeffs');
+    t_hydro_bessels    = time_multiplier_singlebody * extract_runtime(profile_singlebody,'ft_1',bessel_lines);
+    t_hydro_unpack     = time_multiplier_singlebody * extract_runtime(profile_singlebody,'ft_1',unpack_lines);
+    t_hydro_dispersion = time_multiplier_singlebody * extract_runtime(profile_singlebody,'compute_eigen_hydro_coeffs',dispersion_lines);
+    t_hydro_lin_solve  = time_multiplier_singlebody * extract_runtime(profile_singlebody,'compute_eigen_hydro_coeffs',lin_solve_lines);
 
     t_hydro_other = t_hydro - (t_hydro_bessels + t_hydro_unpack + t_hydro_dispersion + t_hydro_lin_solve);
     times_meem = [t_hydro_bessels t_hydro_unpack t_hydro_dispersion t_hydro_lin_solve t_hydro_other];
@@ -66,7 +84,26 @@ function [f1, f2, f3] = module_runtime_compare(p,b)
     cats_hydro = {'MDOcean MEEM','Capytaine'};
     cats_hydro = reordercats(categorical(cats_hydro),cats_hydro);
 
+    % hydro figure, not broken down, log scale
     f2 = figure;
+    h = barh(cats_hydro,sum(times_hydro,2));
+    title('Hydrodynamics Runtime Comparison')
+    subtitle('for a single frequency')
+    ylabel('Runtime (s)')
+    if ~isMATLABReleaseOlderThan('R2024b')
+        h(1).Labels = h(1).YData;
+    end
+    ax = h(1).Parent;
+    split_labels_by_space(ax)
+    h.Parent.XScale = 'log';
+    h.Parent.YGrid  = 'on';
+    improvePlot
+    h.Parent.YMinorGrid = 'off';
+    f2.Position = [100 100 1410 600];
+
+    
+    % hydro figure, broken down by category, not log scale
+    f3 = figure;
     h = barh(cats_hydro,times_hydro,'stacked');
     legend(sections_hydro)
     title('Hydrodynamics Runtime Comparison')
@@ -78,19 +115,19 @@ function [f1, f2, f3] = module_runtime_compare(p,b)
     ax = h(1).Parent;
     split_labels_by_space(ax)
     improvePlot
-    f2.Position = [100 100 1410 600];
+    f3.Position = [100 100 1410 600];
 
     % comparing all modules
     
-    t_struct = extract_runtime(p_singlebody,'structures_one_case');
-    t_geom   = extract_runtime(p_singlebody,'geometry');
-    t_econ   = extract_runtime(p_singlebody,'econ');
+    t_struct = time_multiplier_singlebody * extract_runtime(profile_singlebody,'structures_one_case');
+    t_geom   = time_multiplier_singlebody * extract_runtime(profile_singlebody,'geometry');
+    t_econ   = time_multiplier_singlebody * extract_runtime(profile_singlebody,'econ');
 
     t_modules = [t_geom,t_hydro,t_multibody,t_struct,t_econ];
     name_modules = {'Geometry','Hydrodynamics','Dynamics (2-DOF)','Structures','Economics'};
     name_modules = reordercats(categorical(name_modules),name_modules);
 
-    f3 = figure;
+    f4 = figure;
     h = bar(name_modules,t_modules);
     if ~isMATLABReleaseOlderThan('R2024b')
         h(1).Labels = h(1).YData;
@@ -155,4 +192,3 @@ function split_labels_by_space(ax)
     new_labels = strtrim(sprintf(pattern, labelArray{:}));
     ax.YTickLabel = new_labels;
 end
-
