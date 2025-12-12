01c4bbd2dcce7cf8bbbd5f8057ab352897cc7f41
Rebecca McCabe <chessgirl2400@gmail.com>
2025-12-11 23:34:45 -0500

    rename validation tables from 1a/1b to report and wecsim

--- mdocean/analysis/@Validation/analysis_fcn.m
+++ mdocean/analysis/@Validation/analysis_fcn.m
@@ -1,10 +1,10 @@
 function intermed_result_struct = analysis_fcn(~,~)
     % Run validation analysis
-    [~, ~, ~, ~, tab1a, fig_cost_vs_N_WEC] = validate_nominal_RM3('report');
-    [~,~,~,~,tab1b] = validate_nominal_RM3('wecsim');
+    [~, ~, ~, ~, tab_report, fig_cost_vs_N_WEC] = validate_nominal_RM3('report');
+    [~,~,~,~,tab_wecsim] = validate_nominal_RM3('wecsim');
     
     % Store results for post-processing
-    intermed_result_struct.tab1a = tab1a;
-    intermed_result_struct.tab1b = tab1b;
+    intermed_result_struct.tab_report = tab_report;
+    intermed_result_struct.tab_wecsim = tab_wecsim;
     intermed_result_struct.fig_cost_vs_N_WEC = fig_cost_vs_N_WEC;
 end
--- mdocean/analysis/@Validation/post_process_fcn.m
+++ mdocean/analysis/@Validation/post_process_fcn.m
@@ -5,32 +5,32 @@ function [fig_array,...
                  tab_firstrows,...
                  tab_colspecs] = post_process_fcn(intermed_result_struct)
             
-            tab1a = intermed_result_struct.tab1a;
-            tab1b = intermed_result_struct.tab1b;
+            tab_report = intermed_result_struct.tab_report;
+            tab_wecsim = intermed_result_struct.tab_wecsim;
             
-            % merge table 1a and 1b while preserving row order
-            sharedCols = intersect(tab1a.Properties.VariableNames, tab1b.Properties.VariableNames);
-            tab1a.RowNum = (1:length(tab1a.Properties.RowNames))';
-            tab1b.RowNum = length(tab1a.Properties.RowNames) + (1:length(tab1b.Properties.RowNames))';
+            % merge report and wecsim tables while preserving row order
+            sharedCols = intersect(tab_report.Properties.VariableNames, tab_wecsim.Properties.VariableNames);
+            tab_report.RowNum = (1:length(tab_report.Properties.RowNames))';
+            tab_wecsim.RowNum = length(tab_report.Properties.RowNames) + (1:length(tab_wecsim.Properties.RowNames))';
             
-            tab1 = outerjoin(tab1a, tab1b, 'Keys', [{'RowNum'},sharedCols], 'MergeKeys', true);
-            tab1 = removevars(tab1,'RowNum');
-            tab1.Properties.RowNames = [tab1a.Properties.RowNames; tab1b.Properties.RowNames];
+            tab_validation = outerjoin(tab_report, tab_wecsim, 'Keys', [{'RowNum'},sharedCols], 'MergeKeys', true);
+            tab_validation = removevars(tab_validation,'RowNum');
+            tab_validation.Properties.RowNames = [tab_report.Properties.RowNames; tab_wecsim.Properties.RowNames];
 
             vector_cols = {'capex','capex_design','J_capex_design','capex_struct','capex_PTO','opex','LCOE'};
-            idx_remove = ismember(tab1.Properties.VariableNames,vector_cols); 
-            tab1latex = rows2vars(tab1,'VariableNamingRule','preserve','DataVariables',~idx_remove);
-            tab1latex.OriginalVariableNames = remove_underscores(modify_suffix(tab1latex.OriginalVariableNames));
+            idx_remove = ismember(tab_validation.Properties.VariableNames,vector_cols); 
+            tab_validation_latex = rows2vars(tab_validation,'VariableNamingRule','preserve','DataVariables',~idx_remove);
+            tab_validation_latex.OriginalVariableNames = remove_underscores(modify_suffix(tab_validation_latex.OriginalVariableNames));
             new_names = {'Variable','MDOcean','Actual','Error','MDOcean ','Actual ','Error '};
-            tab1latex = renamevars(tab1latex, tab1latex.Properties.VariableNames, new_names);
+            tab_validation_latex = renamevars(tab_validation_latex, tab_validation_latex.Properties.VariableNames, new_names);
             
             fig_array = intermed_result_struct.fig_cost_vs_N_WEC;
             
-            tab_array_display = {tab1};
-            tab_array_latex = {tab1latex};
+            tab_array_display = {tab_validation};
+            tab_array_latex = {tab_validation_latex};
             tab_firstrows = {'&\multicolumn{3}{c|}{DOE Report RM3 Design \cite{RM3}} & \multicolumn{3}{c}{WEC-Sim RM3 Design} \\'};
             tab_colspecs = {'>{\centering\arraybackslash}p{0.2\linewidth}|c|c|r|c|c|r'};
 
             end_result_struct.validation_complete = true;
-            end_result_struct.validation_table = tab1;
+            end_result_struct.validation_table = tab_validation;
 end
