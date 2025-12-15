e65254f57f3cf06e2ea1eabd283b804ca27a6ed4
Rebecca McCabe <chessgirl2400@gmail.com>
2025-12-15 03:15:15 -0500

    add missing p,b arguments to analysis class

--- mdocean/analysis/@DesignVars/DesignVars.m
+++ mdocean/analysis/@DesignVars/DesignVars.m
@@ -8,7 +8,7 @@ classdef DesignVars < GenericAnalysis
     end
 
     methods (Static)
-        intermed_result_struct = analysis_fcn(~,b)
+        intermed_result_struct = analysis_fcn(p,b)
 
         [fig_array,...
                  tab_array_display,...
