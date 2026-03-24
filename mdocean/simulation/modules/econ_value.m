b270d0b710fc4787f87137e42b317cad748ede40
MadisonDietrich <mjd429@cornell.edu>
2026-03-24 15:14:21 -0400

    econ value and environmental cost wrappers

---
+++ mdocean/simulation/modules/econ_value.m
@@ -0,0 +1,13 @@
+function [LVOE_matrix,LVOE] = econ_value(force_matrix,P_elec)
+
+% calculate capex and opex
+capex = 0;
+
+%fix opex: $/yr of energy revenue on the grid
+alpha_opex = 0.5567;
+opex_per_wec = 1.193e6 * N_WEC^-alpha_opex;
+opex = opex_per_wec * N_WEC;
+
+%calculate the levelized cost matrix
+levelized_cost_matrix = matrix_levelization(capex, opex, FCR, force_matrix);
+[LVOE_matrix,LVOE] = LXOE_func(levelized_cost_matrix,P_elec,efficiency);
