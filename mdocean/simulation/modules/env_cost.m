efcda42c24eb24717d0dd6f31f11471000c2e7dd
Amina Lesbekova <al2588@cornell.edu>
2026-04-07 18:03:25 -0400

    Modify and add the files for environmental cost and value

--- mdocean/simulation/modules/env_cost.m
+++ mdocean/simulation/modules/env_cost.m
@@ -11,3 +11,12 @@ opex = opex_per_wec * N_WEC;
 %calculate the levelized cost matrix
 levelized_cost_matrix = matrix_levelization(capex, opex, FCR, force_matrix);
 [LCOE_matrix,LCOE] = LXOE_func(levelized_cost_matrix,P_elec,efficiency);
+
+function [eco_cost_per_wec, eco_cost_total] = env_cost(steel_mass, distance_from_shore, fiberglass_area, ...
+                                                      eco_cost_steel, eco_cost_fiberglass, eco_cost_distance, ...
+                                                      num_wecs)
+
+eco_cost_per_wec = eco_cost_steel * steel_mass + eco_cost_fiberglass * fiberglass_area + eco_cost_distance * distance_from_shore;
+eco_cost_total   = eco_cost_per_wec * num_wecs;
+
+end
---
+++ mdocean/simulation/modules/env_value.m
@@ -0,0 +1,15 @@
+function [LVOE_matrix, LVOE_env] = env_value(force_matrix, P_elec, FCR, efficiency, years, avoided_co2_tons, scc_case)
+
+if nargin < 7 || isempty(years) || isempty(avoided_co2_tons) || isempty(scc_case)
+    error("env_value(force_matrix,P_elec,FCR,efficiency,years,avoided_co2_tons,scc_case) required.");
+end
+
+[~, ~, avoided_dollars_per_year] = carbon_avoided_value(years, avoided_co2_tons, scc_case);
+
+capex = 0;
+opex  = sum(avoided_dollars_per_year);
+
+levelized_value_matrix = matrix_levelization(capex, opex, FCR, force_matrix);
+[LVOE_matrix, LVOE_env] = LXOE_func(levelized_value_matrix, P_elec, efficiency);
+
+end
