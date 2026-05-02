ceb2e6d761319ed723b80b26fd53559cc1ec63b3
Becca <32020426+rebeccamccabe@users.noreply.github.com>
2026-05-01 23:30:09 -0400

    turn amp sat off for report case

--- mdocean/simulation/run/make_all_cases.m
+++ mdocean/simulation/run/make_all_cases.m
@@ -22,11 +22,11 @@ end
 function p_array = make_case_group(geometry, multibody, runOnlyFewSeaStates)
     p = parameters(geometry);
     p.use_force_sat = false;
-    %if strcmpi(geometry,'wecsim')
-    %    p.use_amp_sat = false;
-    %else
-    p.use_amp_sat = 'true';
-    %end
+    if strcmpi(geometry,'report')
+        p.use_amp_sat = false;
+    else
+        p.use_amp_sat = 'true';
+    end
     p.use_power_sat = false;
     p.use_multibody = multibody;
 
